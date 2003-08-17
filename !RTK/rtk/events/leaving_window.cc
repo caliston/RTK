ca130644b8a9c471b1772cc793073ee63b50eedc
gdshaw <gdshaw@fa0f773a-f822-0410-a9df-e7ef4170f6f5>
2003-08-17 19:06:03 +0000

    Added class rtk::events::leaving_window
    git-svn-id: http://source.riscpkg.org/svn/RTK/trunk@27 fa0f773a-f822-0410-a9df-e7ef4170f6f5

---
+++ !RTK/rtk/events/leaving_window.cc
@@ -0,0 +1,31 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#include "rtk/desktop/component.h"
+#include "rtk/desktop/window.h"
+#include "rtk/events/leaving_window.h"
+
+namespace rtk {
+namespace events {
+
+using rtk::desktop::component;
+using rtk::desktop::window;
+
+leaving_window::leaving_window(window& target):
+	event(target)
+{}
+
+leaving_window::~leaving_window()
+{}
+
+bool leaving_window::deliver(component& dest)
+{
+	handler* h=dynamic_cast<handler*>(&dest);
+	if (h) h->handle_event(*this);
+	return h;
+}
+
+}; /* namespace events */
+}; /* namespace rtk */
---
+++ !RTK/rtk/events/leaving_window.h
@@ -0,0 +1,61 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#ifndef _RTK_EVENTS_LEAVING_WINDOW
+#define _RTK_EVENTS_LEAVING_WINDOW
+
+#include "rtk/events/event.h"
+
+namespace rtk {
+namespace desktop {
+
+class window;
+
+}; /* namespace desktop */
+
+namespace events {
+
+/** A class to represent a RISC OS Pointer_Leaving_Window event.
+ */
+class leaving_window:
+	public event
+{
+public:
+	/** A mixin class for handling leaving_window events.
+	 * If a class wishes to receive leaving_window events then it should
+	 * inherit from this mixin class and provide an implementation for
+	 * handle_event().
+	 */
+	class handler
+	{
+	public:
+		/** Handle leaving_window event.
+		 * @param ev the leaving_window event to be handled
+		 */
+		virtual void handle_event(leaving_window& ev)=0;
+	};
+
+	/** Construct leaving_window event.
+	 * @param target the target of the event (the window left)
+	 */
+	leaving_window(desktop::window& target);
+
+	/** Destroy leaving_window event.
+	 */
+	virtual ~leaving_window();
+
+	/** Get target.
+	 * @return the target of the event (the window left)
+	 */
+	desktop::window* target() const
+		{ return (desktop::window*)target(); }
+protected:
+	virtual bool deliver(desktop::component& dest);
+};
+
+}; /* namespace events */
+}; /* namespace rtk */
+
+#endif
