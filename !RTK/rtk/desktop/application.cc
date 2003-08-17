b5296306a33ee15999802451f264ed8d604dfb67
gdshaw <gdshaw@fa0f773a-f822-0410-a9df-e7ef4170f6f5>
2003-08-17 18:36:54 +0000

    Added class rtk::desktop::application
    git-svn-id: http://source.riscpkg.org/svn/RTK/trunk@24 fa0f773a-f822-0410-a9df-e7ef4170f6f5

---
+++ !RTK/rtk/desktop/application.cc
@@ -0,0 +1,484 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#include <algorithm>
+
+#include "rtk/graphics/vdu_gcontext.h"
+#include "rtk/swi/os.h"
+#include "rtk/swi/wimp.h"
+#include "rtk/os/os.h"
+#include "rtk/os/wimp.h"
+#include "rtk/desktop/icon.h"
+#include "rtk/desktop/window.h"
+#include "rtk/desktop/menu.h"
+#include "rtk/desktop/application.h"
+#include "rtk/events/wimp.h"
+#include "rtk/events/null_reason.h"
+#include "rtk/events/message.h"
+
+namespace rtk {
+namespace desktop {
+
+application::application(const string& name):
+	_name(name),
+	_dbox(0),
+	_dbox_level(0),
+	_quit(false)
+{
+	static int messages[]={0};
+	os::Wimp_Initialise(380,_name.c_str(),messages,0,&_handle);
+}
+
+application::~application()
+{
+	// Removing windows implicitly causes them to deregister.
+	while (_windows.size())
+		_windows.back()->remove();
+	// Similarly for icons.
+	while (_icons.size())
+		_icons.back()->remove();
+	// Similarly for menus.
+	remove_menus(0);
+	remove_menu_data(0);
+}
+
+application* application::as_application()
+{
+	return this;
+}
+
+box application::min_bbox() const
+{
+	// The bounding box of the application object is defined to be the
+	// bounding box of the desktop.
+	int xeigfactor=0;
+	int yeigfactor=0;
+	int xwindlimit=0;
+	int ywindlimit=0;
+	os::OS_ReadModeVariable(swi::XEigFactor,&xeigfactor);
+	os::OS_ReadModeVariable(swi::YEigFactor,&yeigfactor);
+	os::OS_ReadModeVariable(swi::XWindLimit,&xwindlimit);
+	os::OS_ReadModeVariable(swi::YWindLimit,&ywindlimit);
+	return box(0,0,(xwindlimit+1)<<xeigfactor,(ywindlimit+1)<<yeigfactor);
+}
+
+void application::resize() const
+{
+	// Resize child windows.
+	for (vector<window*>::const_iterator i=_windows.begin();
+		i!=_windows.end();++i)
+	{
+		if (!(*i)->size_valid()) (*i)->resize();
+	}
+	// Resize child icons.
+	for (vector<icon*>::const_iterator i=_icons.begin();
+		i!=_icons.end();++i)
+	{
+		if (!(*i)->size_valid()) (*i)->resize();
+	}
+	// Resize menus.
+	for (vector<menu*>::const_iterator i=_menus.begin();i!=_menus.end();++i)
+	{
+		if (!(*i)->size_valid()) (*i)->resize();
+	}
+	// Resize dialogue box.
+	if (_dbox)
+	{
+		if (!_dbox->layout_valid()) _dbox->resize();
+	}
+	inherited::resize();
+}
+
+void application::reformat(const point& origin,const box& bbox)
+{
+	inherited::reformat(origin,bbox);
+	// Reformat child windows.
+	for (vector<window*>::iterator i=_windows.begin();i!=_windows.end();++i)
+	{
+		// The application object does not exercise any influence over
+		// window locations, so they are reformatted to their last known
+		// position and size.
+		if (!(*i)->layout_valid())
+			(*i)->reformat((*i)->origin(),(*i)->bbox());
+	}
+	// Reformat child icons.
+	for (vector<icon*>::iterator i=_icons.begin();i!=_icons.end();++i)
+	{
+		// Reformat so that bottom left-hand corner of each icon is at (0,0).
+		if (!(*i)->layout_valid())
+		{
+			box mcbbox=(*i)->min_bbox();
+			(*i)->reformat(-mcbbox.xminymin(),mcbbox);
+		}
+	}
+	// Reformat menus.
+	for (vector<menu*>::iterator i=_menus.begin();i!=_menus.end();++i)
+	{
+		if (!(*i)->layout_valid())
+		{
+			size_type level=i-_menus.begin();
+			point cpos=(*i)->origin();
+			box mcbbox=(*i)->min_bbox();
+			(*i)->reformat(cpos,mcbbox,level);
+		}
+	}
+	// Reformat dialogue box.
+	if (_dbox)
+	{
+		if (!_dbox->layout_valid())
+		{
+			point cpos=_dbox->origin();
+			box mcbbox=_dbox->min_bbox();
+			_dbox->reformat(cpos,mcbbox,_dbox_level);
+		}
+	}
+}
+
+void application::remove_notify(component& c)
+{
+	// Dynamic casts are used here to avoid unnecessary searches.
+	if (window* w=dynamic_cast<window*>(&c))
+	{
+		// Remove if child window.
+		vector<window*>::iterator f=
+			std::find(_windows.begin(),_windows.end(),w);
+		if (f!=_windows.end())
+		{
+			_windows.erase(f,f+1);
+		}
+		// Remove if dialogue box.
+		if (_dbox==w)
+		{
+			remove_menus(_dbox_level+1);
+			_dbox=0;
+			_dbox_level=0;
+		}
+	}
+	else if (icon* ic=dynamic_cast<icon*>(&c))
+	{
+		// Remove if child icon.
+		vector<icon*>::iterator f=
+			std::find(_icons.begin(),_icons.end(),ic);
+		if (f!=_icons.end())
+		{
+			_icons.erase(f,f+1);
+		}
+	}
+	else if (menu* m=dynamic_cast<menu*>(&c))
+	{
+		// Remove if menu.
+		vector<menu*>::iterator f=
+			std::find(_menus.begin(),_menus.end(),m);
+		if (f!=_menus.end())
+		{
+			int level=f-_menus.begin();
+			remove_menus(level+1);
+			_menus[level]=0;
+		}
+	}
+}
+
+application& application::add(window& w,const point& p)
+{
+	w.origin(p);
+	_windows.push_back(&w);
+	link_child(w);
+	invalidate();
+	return *this;
+}
+
+application& application::add(icon& ic)
+{
+	_icons.push_back(&ic);
+	link_child(ic);
+	invalidate();
+	return *this;
+}
+
+application& application::add(window& w,const point& p,size_type level)
+{
+	// Remove existing menus at or below this level.
+	remove_menus(level);
+	remove_menu_data(level);
+
+	// Remove window from its current parent, if any.
+	w.remove();
+
+	// Set window origin.
+	w.origin(p);
+
+	// Make window a child of this component.
+	_dbox=&w;
+	_dbox_level=level;
+	link_child(w);
+
+	// Invalidate this component (to ensure that the dialogue box
+	// is reformatted) and return.
+	invalidate();
+	return *this;
+}
+
+application& application::add(menu& m,const point& p,size_type level)
+{
+	// Ensure that child pointer array includes this level.
+	if (level>=_menus.size()) _menus.resize(level+1,0);
+
+	// Remove existing menus at or below this level.
+	for (size_type i=level;i!=_menus.size();++i)
+		if (menu* cm=_menus[i]) cm->remove();
+
+	// Remove existing dialogue box if at or below this level.
+	if (_dbox&&(_dbox_level>=level))
+		_dbox->remove();
+
+	// At this point it is an error if the menu to be added has a
+	// parent (because that would make the menu structure cyclic).
+	if (m.parent()) throw string("Menu structure must be acyclic");
+
+	// Set menu origin.
+	m.origin(p);
+
+	// Make menu a child of this component.
+	_menus[level]=&m;
+	link_child(m);
+
+	// Invalidate this component (to ensure that the menu is
+	// reformatted) and return.
+	invalidate();
+	return *this;
+}
+
+void application::run()
+{
+	while (!_quit)
+	{
+		try
+		{
+			// Ensure layout valid before polling Wimp.
+			if (!size_valid()) resize();
+			if (!layout_valid()) reformat(point(0,0),box(0,0,0,0));
+			// Poll Wimp.
+			rtk::graphics::vdu_gcontext::current(0);
+			static os::wimp_block wimpblock;
+			int wimpcode;
+			os::Wimp_Poll(0,wimpblock,0,&wimpcode);
+			// Act on returned event block.
+			deliver_wimp_block(wimpcode,wimpblock);
+			// Send up to one message from queue.
+			if (_message_queue.size())
+			{
+				message msg=_message_queue.front();
+				_message_queue.pop();
+				os::Wimp_SendMessage(msg.wimpcode,*msg.wimpblock,
+					msg.handle,0,0);
+				delete msg.wimpblock;
+			}
+		}
+		// Catch exceptions, display using Wimp_ReportError
+		// (extracting text from exception where suitable method known).
+		catch (std::exception &ex)
+		{
+			os::Wimp_ReportError(0,ex.what(),name().c_str(),0,0);
+		}
+		catch (const char* ex)
+		{
+			os::Wimp_ReportError(0,ex,name().c_str(),0,0);
+		}
+		catch (const string& ex)
+		{
+			os::Wimp_ReportError(0,ex.c_str(),name().c_str(),0,0);
+		}
+		catch (...)
+		{
+			os::Wimp_ReportError(0,"Unknown exception",name().c_str(),0,0);
+		}
+	}
+}
+
+void application::terminate()
+{
+	_quit=true;
+}
+
+void application::deliver_wimp_block(int wimpcode,os::wimp_block& wimpblock)
+{
+	switch (wimpcode)
+	{
+	case 0:
+		{
+			events::null_reason ev(*this);
+			ev.post();
+		}
+		break;
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+	case 8:
+	case 10:
+	case 11:
+	case 12:
+		if (window* w=find_window(wimpblock.word[0]))
+			w->deliver_wimp_block(wimpcode,wimpblock);
+		break;
+	case 6:
+		if (wimpblock.word[3]==-2)
+		{
+			if (icon* ic=find_icon(wimpblock.word[4]))
+				ic->deliver_wimp_block(wimpcode,wimpblock);
+		}
+		else
+		{
+			if (window* w=find_window(wimpblock.word[3]))
+				w->deliver_wimp_block(wimpcode,wimpblock);
+		}
+		break;
+	case 9:
+		if (_menus.size()&&_menus[0])
+			_menus[0]->deliver_wimp_block(wimpcode,wimpblock,0);
+		break;
+	case 17:
+	case 18:
+		deliver_message(wimpcode,wimpblock);
+		break;
+	default:
+		{
+			events::wimp ev(*this,wimpcode,wimpblock);
+			ev.post();
+		}
+		break;
+	}
+}
+
+void application::deliver_message(int wimpcode,os::wimp_block& wimpblock)
+{
+	switch (wimpblock.word[4])
+	{
+	case swi::Message_Quit:
+		{
+			events::quit ev(*this);
+			ev.post();
+		}
+		break;
+	case swi::Message_MenuWarning:
+		if (_menus.size()&&_menus[0])
+			_menus[0]->deliver_wimp_block(wimpcode,wimpblock,0);
+		break;
+	case swi::Message_MenusDeleted:
+		if (window* w=find_window(wimpblock.word[5]))
+		{
+			w->deliver_wimp_block(wimpcode,wimpblock);
+			w->remove();
+		}
+		else if (menu* m=find_menu(wimpblock.word[5]))
+		{
+			m->deliver_wimp_block(wimpcode,wimpblock,0);
+			m->remove();
+			remove_menu_data(0);
+		}
+		break;
+	default:
+		{
+			events::message ev(*this,wimpblock);
+			ev.post();
+		}
+		break;
+	}
+}
+
+void application::send_message(int wimpcode,os::wimp_block& wimpblock,
+	int handle)
+{
+	message msg;
+	msg.wimpcode=wimpcode;
+	msg.wimpblock=&wimpblock;
+	msg.handle=handle;
+	_message_queue.push(msg);
+}
+
+void application::handle_event(events::quit &ev)
+{
+	terminate();
+}
+
+void application::handle_event(events::reopen_menu &ev)
+{
+	if (_menus.size()&&_menus[0])
+	{
+		if (_menus[0]->auto_reopen()) _menus[0]->reopen();
+	}
+}
+
+void application::register_window(window& w)
+{
+	_whandles[w.handle()]=&w;
+}
+
+void application::register_icon(icon& ic)
+{
+	_ihandles[ic.handle()]=&ic;
+}
+
+void application::register_menu_data(util::refcount* mdata,unsigned int level)
+{
+	mdata->inc_count();
+	if (level>=_mdata.size()) _mdata.resize(level+1,0);
+	if (_mdata[level]) _mdata[level]->dec_count();
+	_mdata[level]=mdata;
+}
+
+void application::deregister_window(window& w)
+{
+	_whandles.erase(w.handle());
+}
+
+void application::deregister_icon(icon& ic)
+{
+	_ihandles.erase(ic.handle());
+}
+
+window* application::find_window(int handle) const
+{
+	map<int,window*>::const_iterator f=_whandles.find(handle);
+	return (f!=_whandles.end())?(*f).second:0;
+}
+
+icon* application::find_icon(int handle) const
+{
+	map<int,icon*>::const_iterator f=_ihandles.find(handle);
+	return (f!=_ihandles.end())?(*f).second:0;
+}
+
+menu* application::find_menu(int handle) const
+{
+	return (_menus.size()&&_menus[0]&&(_menus[0]->handle()==handle))?
+		_menus[0]:0;
+}
+
+void application::remove_menus(size_type level)
+{
+	// Remove existing menus at or below this level.
+	// (This is done in reverse order to avoid recursion.)
+	for (size_type i=_menus.size();i>level;--i)
+		if (menu* cm=_menus[i-1]) cm->remove();
+
+	// Remove existing dialogue box if at or below this level.
+	if (_dbox&&(_dbox_level>=level))
+		_dbox->remove();
+}
+
+void application::remove_menu_data(size_type level)
+{
+	// Remove menu data at or below this level.
+	for (size_type i=_mdata.size();i>level;--i)
+	{
+		if (_mdata[i-1]) _mdata[i-1]->dec_count();
+		_mdata[i-1]=0;
+	}
+}
+
+}; /* namespace desktop */
+}; /* namespace rtk */
---
+++ !RTK/rtk/desktop/application.h
@@ -0,0 +1,333 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#ifndef _RTK_DESKTOP_APPLICATION
+#define _RTK_DESKTOP_APPLICATION
+
+#include <queue>
+#include <map>
+#include <string>
+
+#include "rtk/util/refcount.h"
+#include "rtk/desktop/component.h"
+#include "rtk/events/quit.h"
+#include "rtk/events/reopen_menu.h"
+
+namespace rtk {
+namespace os {
+
+union wimp_block;
+
+}; /* namespace os */
+
+namespace desktop {
+
+class window;
+class icon;
+class menu;
+
+/** A class to represent a RISC OS application.
+ * A name must be supplied when the application is constructed.
+ * At present, applications do not correctly implement component::find().
+ */
+class application:
+	public component,
+	public events::quit::handler,
+	public events::reopen_menu::handler
+{
+private:
+	/** The class from which this one is derived. */
+	typedef component inherited;
+public:
+	/** A type for representing indices. */
+	typedef unsigned int size_type;
+private:
+	/** A structure to represent an outbound Wimp message.
+	 * Since this contains a pointer to (as opposed to a copy) of the
+	 * Wimp event block, the latter must remain valid for the lifetime
+	 * of this object. 
+	 */
+	struct message
+	{
+		/** The Wimp event code for the message. */
+		int wimpcode;
+		/** A pointer to the Wimp event block containing the message. */
+		os::wimp_block* wimpblock;
+		/** The task handle to which the message is to be sent. */
+		int handle;
+	};
+
+	/** The RISC OS task handle. */
+	int _handle;
+
+	/** The task name. */
+	string _name;
+
+	/** A map from window handles to window components.
+	 * All descendant windows that currently have a RISC OS window handle
+	 * are included.
+	 */
+	map<int,window*> _whandles;
+
+	/** A map from icon handles to icon components.
+	 * Descendant icons are included if they are not descended from a
+	 * window and they currently have a RISC OS window handle.
+	 */
+	map<int,icon*> _ihandles;
+
+	/** A list of child windows.
+	 * Windows are listed if they are an immediate child of this
+	 * application, but not if they are open as a dialogue box.
+	 */
+	vector<window*> _windows;
+
+	/** The list of child icons.
+	 * Icons are listed if they are an immediate child of this
+	 * application.
+	 */
+	vector<icon*> _icons;
+
+	/** The list of current child menus.
+	 * Menus are listed if they are an immediate child of this
+	 * application.  Their position within this vector corresponds
+	 * to their depth with in the menu heirarchy (first=top).
+	 * It follows that there can be at most one child at each level.
+	 * (Note that the parent of a menu should be either the
+	 * application component or null.)
+	 */
+	vector<menu*> _menus;
+
+	/** The list of current menu data blocks.
+	 * Menu data blocks are listed if there is a possibility that they
+	 * are being used by the Wimp.  Their position within this vector
+	 * corresponds to their depth with in the menu heirarchy (first=top).
+	 * It follows that there can be at most one child at each level.
+	 */
+	vector<util::refcount*> _mdata;
+
+	/** The current dialogue box.
+	 */
+	window* _dbox;
+
+	/** The current dialogue box level.
+	 */
+	size_type _dbox_level;
+
+	/** The outbound message queue.
+	 * Messages are added to the back of the queue and taken from the
+	 * front.  Transmission occurs automatically once a message has
+	 * been put in the queue, but is limited to a maximum of one
+	 * message per polling interval
+	 */
+	queue<message> _message_queue;
+
+	/** The quit flag.
+	 * True if this application should terminate gracefully when control
+	 * returns to the main polling loop.
+	 */
+	bool _quit;
+public:
+	/** Construct application.
+	 * @param name the name of this application
+	 */
+	application(const string& name);
+
+	/** Destroy application. */
+	virtual ~application();
+
+	virtual application* as_application();
+	virtual box min_bbox() const;
+	virtual void resize() const;
+	virtual void reformat(const point& origin,const box& bbox);
+protected:
+	virtual void remove_notify(component& c);
+public:
+	/** Add icon.
+	 * When an icon is added to an application it appears on the icon bar.
+	 * @param ic the icon to be added
+	 */
+	application& add(icon& ic);
+
+	/** Add window.
+	 * @param w the window to be added
+	 * @param p the required initial origin
+	 */
+	application& add(window& w,const point& p);
+
+	/** Add window as dialogue box.
+	 * @param w the window to be added as a dialogue box
+	 * @param p the required initial origin
+	 * @param level the level within the menu heirarchy (0=top level,
+	    add 1 for each level down)
+	 */
+	application& add(window& w,const point& p,size_type level);
+
+	/** Add menu.
+	 * @param m the menu to be added
+	 * @param p the required position of the origin
+	 * @param level the level within the menu heirarchy (0=top level,
+	    add 1 for each level down)
+	 */
+	application& add(menu& m,const point& p,size_type level=0);
+
+	/** Get name.
+	 * The name of an application is fixed when it is constructed.
+	 * @return the name of this application
+	 */
+	string& name()
+		{ return _name; }
+
+	/** Run this application.
+	 * This method contains the main polling loop.  It should be
+	 * called once from main().
+	 */
+	void run();
+
+	/** Terminate this application.
+	 * This method causes the application to terminate gracefully
+	 * when control returns to the main polling loop.  Take case
+	 * not to confuse this method with the global standard library
+	 * function terminate(), which performs an immediate non-graceful
+	 * termination.
+	 */
+	void terminate();
+
+	/** Default handler for quit events.
+	 * If this handler remains registered then the application
+	 * will terminate gracefully when it receives a Message_Quit.
+	 * Well-behaved applications are obliged to do this, so it
+	 * should not be necessary to override this handler.  Any
+	 * required clean-up actions can (and should) be performed by
+	 * the relevant destructors.
+	 * @param ev the quit event
+	 */
+	void handle_event(events::quit& ev);
+
+	/** Default handler for reopen-menu events.
+	 * If this handler remains registered then the application
+	 * will re-open the current menu tree when a menu selection
+	 * is made using the adjust button.  This behaviour can be
+	 * disabled by setting the auto-reopen attribute of the menu
+	 * to false.
+	 * @param ev the reopen_menu event
+	 */
+	void handle_event(events::reopen_menu& ev);
+
+	/** Deliver Wimp event block.
+	 * @internal
+	 * The event block is converted into a suitable event object and
+	 * posted to the appropriate target.
+	 * @param wimpcode the Wimp event code
+	 * @param wimpblock the Wimp event block
+	 */
+	void deliver_wimp_block(int wimpcode,os::wimp_block& wimpblock);
+
+	/** Deliver Wimp message.
+	 * @internal
+	 * The event block (which should contain a message) is converted
+	 * into a suitable event object and posted to the appropriate target.
+	 * @param wimpcode the Wimp event code
+	 * @param wimpblock the Wimp event block
+	 */
+	void deliver_message(int wimpcode,os::wimp_block& wimpblock);
+
+	/** Send Wimp message.
+	 * The event block must be dynamically allocated on the heap.
+	 * It is automatically deleted once the message has been sent.
+	 * @param wimpcode the Wimp event code
+	 * @param wimpblock the Wimp event block
+	 * @param handle the task handle to which the message is to be sent
+	 */
+	void send_message(int wimpcode,os::wimp_block& wimpblock,int handle);
+
+	/** Register window.
+	 * A window should be registered while it has a Wimp window handle
+	 * in its possession.  This allows it to receive events from the
+	 * Wimp.  Windows should register with the application from which
+	 * they are descended (even if they are more closely descended from
+	 * another window).
+	 * @param w the window to be registered
+	 */
+	void register_window(window& w);
+
+	/** Register icon.
+	 * An icon should be registered while it has a Wimp icon handle in
+	 * its possession.  This allows it to receive events from the Wimp.
+	 * Icons should register with the application or window from which
+	 * they are most closely descended.
+	 * @param ic the icon to be registered.
+	 */
+	void register_icon(icon& ic);
+
+	/** Register menu data.
+	 * A menu data block should be registered whenever it is passed
+	 * to the Wimp.  The application object is responsible for
+	 * de-registering it, and will not do so while there is a
+	 * possibility that the Wimp is still using it.
+	 *
+	 * The reference count of the data block is incremented on
+	 * registration and decremented on de-registration.
+	 * @param mdata the menu data block to be registered
+	 * @param level the level within the menu heirarchy (0=top level,
+	    add 1 for each level down)
+	 */
+	void register_menu_data(util::refcount* mdata,unsigned int level);
+
+	/** Deregister window.
+	 * This must be done while w->handle() returns the same value as
+	 * when the window was registered.
+	 * @param w the window to be deregistered.
+	 */
+	void deregister_window(window& w);
+
+	/** Deregister icon.
+	 * This must be done while ic->handle() returns the same value as
+	 * when the icon was registered.
+	 * @param ic the icon to be deregistered.
+	 */
+	void deregister_icon(icon& ic);
+
+	/** Find window.
+	 * @param handle the window handle
+	 * @return the window, or 0 if not found
+	 */
+	window* find_window(int handle) const;
+
+	/** Find icon.
+	 * @param handle the icon handle
+	 * @return the icon, or 0 if not found
+	 */
+	icon* find_icon(int handle) const;
+
+	/** Find menu.
+	 * At present, only the top-level menu is found by this function.
+	 * @param handle the address of the menu data
+	 * @return the menu, or 0 if not found
+	 */
+	menu* find_menu(int handle) const;
+
+	/** Get RISC OS task handle.
+	 * @return the RISC OS task handle
+	 */
+	int handle()
+		{ return _handle; }
+private:
+	/** Remove menus and dialogue boxes at or below given level.
+	 * @param the uppermost level to be removed (0=top level,
+	 *  add 1 for each level down)
+	 */
+	void remove_menus(size_type level);
+
+	/** Remove menu data for menus at or below given level.
+	 * @param the uppermost level to be removed (0=top level,
+	 *  add 1 for each level down)
+	 */
+	void remove_menu_data(size_type level);
+};
+
+}; /* namespace desktop */
+}; /* namespace rtk */
+
+#endif
