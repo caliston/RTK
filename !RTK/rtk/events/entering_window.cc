dee0e9c9cccd4a04b30efdc3edca6f01c5159225
gdshaw <gdshaw@fa0f773a-f822-0410-a9df-e7ef4170f6f5>
2003-08-17 19:04:40 +0000

    Added class rtk::events::entering_window
    git-svn-id: http://source.riscpkg.org/svn/RTK/trunk@26 fa0f773a-f822-0410-a9df-e7ef4170f6f5

---
+++ !RTK/rtk/events/entering_window.cc
@@ -0,0 +1,31 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#include "rtk/desktop/component.h"
+#include "rtk/desktop/window.h"
+#include "rtk/events/entering_window.h"
+
+namespace rtk {
+namespace events {
+
+using rtk::desktop::component;
+using rtk::desktop::window;
+
+entering_window::entering_window(window& target):
+	event(target)
+{}
+
+entering_window::~entering_window()
+{}
+
+bool entering_window::deliver(component& dest)
+{
+	handler* h=dynamic_cast<handler*>(&dest);
+	if (h) h->handle_event(*this);
+	return h;
+}
+
+}; /* namespace events */
+}; /* namespace rtk */
---
+++ !RTK/rtk/events/entering_window.h
@@ -0,0 +1,61 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#ifndef _RTK_EVENTS_ENTERING_WINDOW
+#define _RTK_EVENTS_ENTERING_WINDOW
+
+#include "rtk/events/event.h"
+
+namespace rtk {
+namespace desktop {
+
+class window;
+
+}; /* namespace desktop */
+
+namespace events {
+
+/** A class to represent a RISC OS Pointer_Entering_Window event.
+ */
+class entering_window:
+	public event
+{
+public:
+	/** A mixin class for handling entering_window events.
+	 * If a class wishes to receive entering_window events then it should
+	 * inherit from this mixin class and provide an implementation for
+	 * handle_event().
+	 */
+	class handler
+	{
+	public:
+		/** Handle entering_window event.
+		 * @param ev the entering_window event to be handled
+		 */
+		virtual void handle_event(entering_window& ev)=0;
+	};
+
+	/** Construct entering_window event.
+	 * @param target the target of the event (the window entered)
+	 */
+	entering_window(desktop::window& target);
+
+	/** Destroy entering_window event.
+	 */
+	virtual ~entering_window();
+
+	/** Get target.
+	 * @return the target of the event (the window entered)
+	 */
+	desktop::window* target() const
+		{ return (desktop::window*)target(); }
+protected:
+	virtual bool deliver(desktop::component& dest);
+};
+
+}; /* namespace events */
+}; /* namespace rtk */
+
+#endif
