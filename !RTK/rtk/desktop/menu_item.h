cb06e5a372adb2459f1b6164365e177ae152bc88
gdshaw <gdshaw@fa0f773a-f822-0410-a9df-e7ef4170f6f5>
2003-08-20 05:37:51 +0000

    Added declaration for class rtk::desktop::menu_item
    git-svn-id: http://source.riscpkg.org/svn/RTK/trunk@32 fa0f773a-f822-0410-a9df-e7ef4170f6f5

---
+++ !RTK/rtk/desktop/menu_item.h
@@ -0,0 +1,193 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#ifndef _RTK_DESKTOP_MENU_ITEM
+#define _RTK_DESKTOP_MENU_ITEM
+
+#include "rtk/desktop/icon.h"
+
+namespace rtk {
+namespace desktop {
+
+class window;
+class menu;
+
+/** A class to represent a RISC OS menu item. */
+class menu_item:
+	public icon
+{
+private:
+	/** The class from which this one is derived. */
+	typedef icon inherited;
+
+	/** A union used to hold an attached menu or dialogue box. */
+	union
+	{
+		/** The menu that is currently attached to this component, if any.
+		 * This variable is meaningful if _has_menu is true.
+		 */
+		menu* _submenu;
+		/** The dialogue box that is currently attached to this component,
+		 * if any.  This variable is meaningful if _has_dbox is true.
+		 */
+		window* _dbox;
+	};
+
+	/** The has-submenu flag.
+	 * True if _submenu points to a menu that is currently attached to
+	 * this menu item.  (The submenu is not a child, so can be attached to
+	 * any number of menu items.)
+	 */
+	bool _has_submenu:1;
+
+	/** The has-dbox flag.
+	 * True if _dbox points to a dialogue box window that is currently
+	 * attached to this menu item.  (The dialogue box is not a child,
+	 * so can be attached to any number of menu items.)
+	 */
+	bool _has_dbox:1;
+
+	/** The separator flag.
+	 * True if this menu item is followed by a separator, otherwise false.
+	 */
+	bool _separator:1;
+public:
+	/** Construct menu item.
+	 * By default, a menu item:
+	 * <UL>
+	 * <LI>has a background colour of 0;</LI>
+	 * <LI>is not followed by a separator; and</LI>
+	 * <LI>keeps the menu tree open if clicked with adjust.</LI>
+	 * </UL>
+	 * Other default attributes are as for an icon.
+	 */
+	menu_item();
+
+	/** Destroy menu item. */
+	virtual ~menu_item();
+public:
+	/** Get submenu attached to this menu item.
+	 * @return the attached submenu, or 0 if there is none
+	 */
+	menu* submenu();
+
+	/** Attach submenu.
+	 * Any existing submenu or dialogue box is detached.
+	 * Attaching a submenu does not make it a child of this menu item.
+	 * It becomes a child of the application when it is opened by the
+	 * user, and ceases to be a child when it is closed.  This behaviour
+	 * allows one submenu to be shared by several menu items.
+	 * @param submenu the submenu to be attached
+	 * @return a reference to this
+	 */
+	menu_item& attach_submenu(menu& submenu);
+
+	/** Detach submenu.
+	 * If there is no attached submenu then no action is taken.
+	 * @return a reference to this
+	 */
+	menu_item& detach_submenu();
+
+	/** Get dialogue box attached to this menu item.
+	 * @return the attached dialogue box, or 0 is there is none
+	 */
+	window* dbox();
+
+	/** Attach dialogue box.
+	 * Any existing submenu or dialogue box is detached.
+	 * Attaching a dialogue box does not make it a child of this menu
+	 * item.  It becomes a child of the application when it is opened
+	 * by the user, and ceases to be a child when it is closed.  This
+	 * behaviour allows one dialogue box to be shared between several
+	 * menu items.
+	 * @param dbox the dialogue box to be attached
+	 * @return a reference to this
+	 */
+	menu_item& attach_dbox(window& dbox);
+
+	/** Detach dialogue box.
+	 * If there is no attached dialogue box then no action is taken.
+	 * @return a reference to this
+	 */
+	menu_item& detach_dbox();
+
+	/** Get separator flag.
+	 * @return true if this menu item is followed by a separator,
+	 *  otherwise false
+	 */
+	bool separator() const
+		{ return _separator; }
+
+	/** Set separator flag.
+	 * @param value true if this item is to be followed by a separator,
+	 *  otherwise false
+	 */
+	menu_item& separator(bool value);
+
+	/** Get menu flags.
+	 * @return menu flags for use by the Wimp
+	 */
+	int menu_flags() const;
+
+	/** Get submenu or dialogue box handle.
+	 * @internal
+	 * If this menu item has an attached submenu then return the
+	 * address of the associated menu data block, otherwise if it
+	 * has an attached dialogue box then return the associated
+	 * window handle, otherwise return 0.
+	 * If necessary, menu::create() or window::create() will be
+	 * called to ensure that the menu has a data block or window
+	 * has a handle.  Where this behaviour is not desirable
+	 * use dummy_submenu_handle() instead.
+	 * @return the submenu or dialogue box handle, or 0 if none
+	 */
+	int submenu_handle() const;
+
+	/** Get dummy submenu or dialogue box handle.
+	 * @internal
+	 * If this menu item has an attached submenu then return the
+	 * address of the associated menu data block, otherwise if it
+	 * has an attached dialogue box then return the associated
+	 * window handle, otherwise return 0.
+	 * If the menu does not have a data block, or window does not
+	 * have a handle, a dummy value is returned which is suitable
+	 * for use when populating a menu data block (provided it is
+	 * only used to generate Message_MenuWarning events).
+	 * @return a dummy submenu or dialogue box handle, or 0 if none
+	 */
+	int dummy_submenu_handle() const;
+
+	/** Deliver Wimp event block.
+	 * @internal
+	 * The event block is converted into a suitable event object and
+	 * posted to the appropriate target (which should be this menu, one
+	 * of its children, or a component lower in the menu heirarchy).
+	 * @param wimpcode the Wimp event code
+	 * @param wimpblock the Wimp event block
+	 * @param level the level within the menu heirarchy (0=top level,
+	    add 1 for each level down)
+	 */
+	void deliver_wimp_block(int wimpcode,os::wimp_block& wimpblock,
+		unsigned int level);
+
+	/** Deliver Wimp message.
+	 * @internal
+	 * The event block (which should contain a message) is converted
+	 * into a suitable event object and posted to the appropriate target
+	 * (which should be this menu, one of its children, or a component
+	 * lower in the menu heirarchy).
+	 * @param wimpcode the Wimp event code
+	 * @param wimpblock the Wimp event block
+	 * @param level the level within the menu heirarchy (0=top level,
+	    add 1 for each level down)
+	 */
+	void deliver_message(int wimpcode,os::wimp_block& wimpblock,
+		unsigned int level);
+};
+
+}; /* namespace desktop */
+}; /* namespace rtk */
+
+#endif
