bbddede91b5069b58532d87cbb84a02135d1ce41
gdshaw <gdshaw@fa0f773a-f822-0410-a9df-e7ef4170f6f5>
2003-08-20 05:58:30 +0000

    Added class rtk::desktop::column_layout
    git-svn-id: http://source.riscpkg.org/svn/RTK/trunk@42 fa0f773a-f822-0410-a9df-e7ef4170f6f5

---
+++ !RTK/rtk/desktop/column_layout.cc
@@ -0,0 +1,255 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#include <algorithm>
+#include <functional>
+
+#include "rtk/util/divider.h"
+#include "rtk/graphics/gcontext.h"
+#include "rtk/desktop/column_layout.h"
+
+namespace rtk {
+namespace desktop {
+
+using rtk::util::divider;
+
+column_layout::column_layout(size_type ycells):
+	_components(ycells,0),
+	_ymax(ycells+1,0),
+	_ygap(0)
+{}
+
+column_layout::~column_layout()
+{
+	for (vector<component*>::iterator i=_components.begin();
+		i!=_components.end();++i)
+	{
+		if (component* c=*i) c->remove();
+	}
+}
+
+box column_layout::auto_bbox() const
+{
+	// Determine number of cells.
+	size_type ycells=_components.size();
+
+	// Reset x-baseline set.
+	_xbs=xbaseline_set();
+
+	// Request min_bbox for each cell.  Incorporate into x-baseline set
+	// and total height.
+	int ysize=0;
+	vector<component*>::const_iterator i=_components.begin();
+	for (size_type y=0;y!=ycells;++y)
+	{
+		if (component* c=*i++)
+		{
+			box mcbbox=c->min_bbox();
+			_xbs.add(mcbbox,c->xbaseline());
+			ysize+=mcbbox.ysize();
+		}
+	}
+
+	// Calculate combined width.
+	int xsize=_xbs.xsize();
+
+	// Add gaps to total height.
+	if (ycells) ysize+=(ycells-1)*_ygap;
+
+	// Add margin to width and height.
+	xsize+=_margin.xsize();
+	ysize+=_margin.ysize();
+
+	// Construct minimum bounding box, with respect to top left-hand
+	// corner of layout.
+	box abbox(0,-ysize,xsize,0);
+
+	// Translate to external origin and return.
+	abbox-=external_origin(abbox,xbaseline_left,ybaseline_top);
+	return abbox;
+}
+
+component* column_layout::find(const point& p) const
+{
+	vector<int>::const_iterator yf=
+		lower_bound(_ymax.begin(),_ymax.end(),p.y()-1,greater<int>());
+	if (yf==_ymax.begin()) return 0;
+	size_type y=(yf-_ymax.begin())-1;
+	component* c=_components[y];
+	if (!c) return 0;
+	box cbbox=c->bbox()+c->origin();
+	if (!(p<=cbbox)) return 0;
+	return c;
+}
+
+box column_layout::bbox() const
+{
+	return _bbox;
+}
+
+void column_layout::resize() const
+{
+	for (vector<component*>::const_iterator i=_components.begin();
+		i!=_components.end();++i)
+	{
+		if (component* c=*i) c->resize();
+	}
+	inherited::resize();
+}
+
+void column_layout::reformat(const point& origin,const box &bbox)
+{
+	// Determine number of cells.
+	size_type ycells=_components.size();
+
+	// Update origin and bounding box of this component, force redraw
+	// if necessary.  (This must happen before reformat() is called for
+	// any children.)
+	bool moved=(origin!=this->origin())||(bbox!=this->bbox());
+	if (moved) force_redraw();
+	_bbox=bbox;
+	inherited::reformat(origin,bbox);
+	if (moved) force_redraw();
+
+	// Remove margin.
+	box ibox(_bbox-_margin);
+
+	box abbox=auto_bbox();
+
+	// Calculate excess.
+	int yexcess=_bbox.ysize()-abbox.ysize();
+	if (yexcess<0) yexcess=0;
+	int yspread=abbox.ysize();
+	yspread-=_margin.ysize();
+	if (ycells) yspread-=(ycells-1)*_ygap;
+	if (yspread<0) yspread=0;
+	divider ydiv(yexcess,yspread);
+
+	// Set maximum y-coordinate for each cell.
+	int ypos=ibox.ymax();
+	_ymax[0]=ypos;
+	for (size_type y=0;y!=ycells;++y)
+	{
+		// Get minimum bounding box for cell, if it is occupied.
+		component* c=_components[y];
+		box mcbbox=(c)?c->min_bbox():box();
+
+		// Set maximum y-coordinate for next cell.
+		int ysize=mcbbox.ysize();
+		ypos-=ysize+ydiv(ysize);
+		ypos-=_ygap;
+		_ymax[y+1]=ypos;
+	}
+
+	// Place children.
+	for (size_type y=0;y!=ycells;++y)
+	{
+		if (component* c=_components[y])
+		{
+			// Create y-baseline set for just this cell.
+			ybaseline_set ybs;
+			ybs.add(c->min_bbox(),c->ybaseline());
+
+			// Construct bounding box for cell with respect to origin
+			// of layout.
+			box cbbox(ibox.xmin(),_ymax[y+1]+_ygap,ibox.xmax(),_ymax[y]);
+
+			// Calculate offset from top left-hand corner of cell to
+			// origin of child.
+			int xoffset=_xbs.offset(xbaseline_left,c->xbaseline(),
+				cbbox.xsize());
+			int yoffset=ybs.offset(ybaseline_bottom,c->ybaseline(),
+				cbbox.ysize());
+			point coffset(xoffset,yoffset);
+
+			// Calculate origin of cell with respect to origin of layout.
+			point cpos(cbbox.xminymin()+coffset);
+
+			// Reformat child.
+			c->reformat(cpos,cbbox-cpos);
+		}
+	}
+}
+
+void column_layout::unformat()
+{
+	for (vector<component*>::iterator i=_components.begin();
+		i!=_components.end();++i)
+	{
+		if (component* c=*i) c->unformat();
+	}
+}
+
+void column_layout::redraw(gcontext& context,const box& clip)
+{
+	for (vector<component*>::iterator i=_components.begin();
+		i!=_components.end();++i)
+	{
+		if (component* c=*i)
+		{
+			point cpos=c->origin();
+			context+=cpos;
+			c->redraw(context,clip+cpos);
+			context-=cpos;
+		}
+	}
+	inherited::redraw(context,clip);
+}
+
+void column_layout::remove_notify(component& c)
+{
+	vector<component*>::iterator f=
+		std::find(_components.begin(),_components.end(),&c);
+	if (f!=_components.end())
+	{
+		*f=0;
+		invalidate();
+	}
+}
+
+column_layout& column_layout::cells(size_type ycells)
+{
+	for (size_type i=min(ycells,_components.size());i!=_components.size();++i)
+		if (component* c=_components[i]) c->remove();
+	_components.resize(ycells,0);
+	_ymax.resize(ycells,0);
+	invalidate();
+	return *this;
+}
+
+column_layout& column_layout::add(component& c,size_type y)
+{
+	if (y==npos) y=_components.size();
+	if (y>=_components.size()) cells(y+1);
+	if (_components[y]) _components[y]->remove();
+	_components[y]=&c;
+	link_child(c);
+	invalidate();
+	return *this;
+}
+
+column_layout& column_layout::ygap(int ygap)
+{
+	_ygap=ygap;
+	invalidate();
+	return *this;
+}
+
+column_layout& column_layout::margin(const box& margin)
+{
+	_margin=margin;
+	invalidate();
+	return *this;
+}
+
+column_layout& column_layout::margin(int margin)
+{
+	_margin=box(-margin,-margin,margin,margin);
+	invalidate();
+	return *this;
+}
+
+}; /* namespace desktop */
+}; /* namespace rtk */
---
+++ !RTK/rtk/desktop/column_layout.h
@@ -0,0 +1,137 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#ifndef _RTK_DESKTOP_COLUMN_LAYOUT
+#define _RTK_DESKTOP_COLUMN_LAYOUT
+
+#include <vector>
+
+#include "rtk/desktop/sizeable_component.h"
+
+namespace rtk {
+namespace desktop {
+
+/** A layout class for arranging components in a column.
+ * It is possible to specify the gap to be placed between cells and the
+ * margin to be placed around the layout as a whole.  Cells that share
+ * the same vertical baseline are aligned with each other. The layout
+ * will expand to fill any extra space made available to it. Note that
+ * the algorithm used to do this is implementation-dependent, so may
+ * change in future versions of this library.
+ * @todo The redraw() function should only redraw cells that lie within
+ *  the clip rectangle, whereas at present it redraws everthing.
+ */
+class column_layout:
+	public sizeable_component
+{
+private:
+	/** The class from which this one is derived. */
+	typedef sizeable_component inherited;
+public:
+	/** A type for representing cell counts or indices. */
+	typedef unsigned int size_type;
+
+	/** A null value for use in place of a cell index. */
+	static const size_type npos=static_cast<size_type>(-1);
+private:
+	/** Vector containing child pointers for each cell. */
+	vector<component*> _components;
+
+	/** Vector containing position of top edge of each cell with
+	 * respect to origin of layout.  The position of the bottom edge
+	 * is obtained by adding _ygap from the top edge of the
+	 * following cell.  A hypothetical value for _ymax[ycells()] is
+	 * included in the vector so that the position of the bottom edge
+	 * of the last cell can be determined. */
+	vector<int> _ymax;
+
+	/** The cached x-baseline set for the layout. */
+	mutable xbaseline_set _xbs;
+
+	/** The size of gap between cells. */
+	int _ygap;
+
+	/** The margin to be placed around the whole layout. */
+	box _margin;
+
+	/** The current bounding box. */
+	box _bbox;
+public:
+	/** Construct column layout.
+	 * @param ycells the required number of cells (defaults to 0)
+	 */
+	column_layout(size_type ycells=0);
+
+	/** Destroy column layout. */
+	virtual ~column_layout();
+
+	virtual box auto_bbox() const;
+	virtual component* find(const point& p) const;
+	virtual box bbox() const;
+	virtual void resize() const;
+	virtual void reformat(const point& origin,const box &bbox);
+	virtual void unformat();
+	virtual void redraw(gcontext& context,const box& clip);
+protected:
+	virtual void remove_notify(component& c);
+public:
+	/** Get number of cells.
+	 * @return the number of cells
+	 */
+	size_type ycells() const
+		{ return _components.size(); }
+
+	/** Set number of cells.
+	 * @param ycells the required number of cells
+	 * @return a reference to this
+	 */
+	column_layout& cells(size_type ycells);
+
+	/** Add component to layout.
+	 * If a component already exists at the specified position
+	 * then it is replaced.
+	 * @param c the component to be added
+	 * @param y the cell index (defaults to npos, meaning new cell)
+	 * @return a reference to this
+	 */
+	column_layout& add(component& c,size_type y=npos);
+
+	/** Get size of gap between cells.
+	 * @return the size of gap between cells
+	 */
+	int ygap() const
+		{ return _ygap; }
+
+	/** Set size of gap between cells.
+	 * @param ygap the required gap between cells
+	 * @return a reference to this
+	 */
+	column_layout& ygap(int ygap);
+
+	/** Get margin around layout.
+	 * @return a box indicating the margin width for each side of the layout
+	 */
+	const box& margin() const
+		{ return _margin; }
+
+	/** Set margin around layout.
+	 * @param margin a box specifying the required margin width for each
+	 *  side of the layout
+	 * @return a reference to this
+	 */
+	column_layout& margin(const box& margin);
+
+	/** Set margin around layout.
+	 * @param margin an integer specifying the required margin width for
+	 *  all sides of the layout
+	 * @return a reference to this
+	 */
+	column_layout& margin(int margin);
+};
+
+}; /* namespace desktop */
+}; /* namespace rtk */
+
+#endif
