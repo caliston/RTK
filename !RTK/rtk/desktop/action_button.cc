5b10243c52c57f00735d6c32624f409fb6c7d504
gdshaw <gdshaw@fa0f773a-f822-0410-a9df-e7ef4170f6f5>
2003-08-20 05:46:33 +0000

    Added class rtk::desktop::action_button
    git-svn-id: http://source.riscpkg.org/svn/RTK/trunk@36 fa0f773a-f822-0410-a9df-e7ef4170f6f5

---
+++ !RTK/rtk/desktop/action_button.cc
@@ -0,0 +1,21 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#include "rtk/desktop/action_button.h"
+
+namespace rtk {
+namespace desktop {
+
+action_button::action_button()
+{
+	validation("R5,3");
+	border(true);
+	hcentre(true).vcentre(true);
+	fcolour(7).bcolour(1);
+	button(3);
+}
+
+}; /* namespace desktop */
+}; /* namespace rtk */
---
+++ !RTK/rtk/desktop/action_button.h
@@ -0,0 +1,39 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#ifndef _RTK_DESKTOP_ACTION_BUTTON
+#define _RTK_DESKTOP_ACTION_BUTTON
+
+#include "rtk/desktop/icon.h"
+
+namespace rtk {
+namespace desktop {
+
+/** A class to represent a RISC OS action button.
+ * The only difference between this class and a generic icon is the
+ * default values set by the constructor.
+ */
+class action_button:
+	public icon
+{
+public:
+	/** Construct action button.
+	 * By default an action button:
+	 * - has a validation string of "R5,3";
+	 * - has a border;
+	 * - is horizontally centred;
+	 * - is vertically centred;
+	 * - has a foreground colour of 7;
+	 * - has a background colour of 1; and
+	 * - has a button type of 3.
+	 * All other attributes are as for an icon.
+	 */
+	action_button();
+};
+
+}; /* namespace desktop */
+}; /* namespace rtk */
+
+#endif
