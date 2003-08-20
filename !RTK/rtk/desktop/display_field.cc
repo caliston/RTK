7e20afd415733d381b2b90e0c894b858ad90e951
gdshaw <gdshaw@fa0f773a-f822-0410-a9df-e7ef4170f6f5>
2003-08-20 05:49:12 +0000

    Added class rtk::desktop::display_field
    git-svn-id: http://source.riscpkg.org/svn/RTK/trunk@38 fa0f773a-f822-0410-a9df-e7ef4170f6f5

---
+++ !RTK/rtk/desktop/display_field.cc
@@ -0,0 +1,20 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#include "rtk/desktop/display_field.h"
+
+namespace rtk {
+namespace desktop {
+
+display_field::display_field()
+{
+	validation("R2");
+	border(true);
+	hcentre(true).vcentre(true);
+	fcolour(7).bcolour(1);
+}
+
+}; /* namespace desktop */
+}; /* namespace rtk */
---
+++ !RTK/rtk/desktop/display_field.h
@@ -0,0 +1,38 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#ifndef _RTK_DESKTOP_DISPLAY_FIELD
+#define _RTK_DESKTOP_DISPLAY_FIELD
+
+#include "rtk/desktop/icon.h"
+
+namespace rtk {
+namespace desktop {
+
+/** A class to represent a RISC OS display field.
+ * The only difference between this class and a generic icon is the
+ * default values set by the constructor.
+ */
+class display_field:
+	public icon
+{
+public:
+	/** Construct display field.
+	 * By default a display field:
+	 * - has a validation string of "R2";
+	 * - has a border;
+	 * - is horizontally centred;
+	 * - is vertically centred;
+	 * - has a foreground colour of 7; and
+	 * - has a background colour of 1.
+	 * All other attributes are as for an icon.
+	 */
+	display_field();
+};
+
+}; /* namespace desktop */
+}; /* namespace rtk */
+
+#endif
