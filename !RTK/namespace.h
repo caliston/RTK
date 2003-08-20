b62e9cfdc2159c5e20dfb5831fa4e74b573885de
gdshaw <gdshaw@fa0f773a-f822-0410-a9df-e7ef4170f6f5>
2003-08-20 18:05:37 +0000

    Added namespace documentation
    git-svn-id: http://source.riscpkg.org/svn/RTK/trunk@49 fa0f773a-f822-0410-a9df-e7ef4170f6f5

---
+++ !RTK/namespace.h
@@ -0,0 +1,27 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use subject to the GNU LGPL.
+// (For details see the file !RTK.Copyright.)
+
+/// The namespace used to hold the RISC OS Toolkit (RTK).
+namespace rtk {
+
+/// The namespace used to hold RTK utility classes.
+namespace util {};
+
+/// The namespace used to hold the RTK graphics library.
+namespace graphics {};
+
+/// The namespace used to hold the RTK low-level RISC OS interface library.
+namespace swi {};
+
+/// The namespace used to hold the RTK high-level RISC OS interface library.
+namespace os {};
+
+/// The namespace used to hold RTK desktop components.
+namespace desktop {};
+
+/// The namespace used to hold RTK event objects.
+namespace events {};
+
+}; /* namespace rtk */
