0db4134dfb8b82410f7b350bb02ca453416a78fa
gdshaw <gdshaw@fa0f773a-f822-0410-a9df-e7ef4170f6f5>
2003-08-20 05:56:28 +0000

    Added class rtk::desktop::grid_layout
    git-svn-id: http://source.riscpkg.org/svn/RTK/trunk@40 fa0f773a-f822-0410-a9df-e7ef4170f6f5

---
+++ !RTK/rtk/desktop/grid_layout.cc
@@ -0,0 +1,349 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#include <algorithm>
+#include <functional>
+
+#include "rtk/util/divider.h"
+#include "rtk/graphics/gcontext.h"
+#include "rtk/desktop/grid_layout.h"
+
+namespace rtk {
+namespace desktop {
+
+using rtk::util::divider;
+
+grid_layout::grid_layout(size_type xcells,size_type ycells):
+	_xcells(xcells),
+	_ycells(ycells),
+	_components(xcells*ycells,0),
+	_xmin(xcells+1,0),
+	_ymax(ycells+1,0),
+	_xbs(xcells),
+	_ybs(ycells),
+	_xgap(0),
+	_ygap(0)
+{}
+
+grid_layout::~grid_layout()
+{
+	for (vector<component*>::iterator i=_components.begin();
+		i!=_components.end();++i)
+	{
+		if (component* c=*i) c->remove();
+	}
+}
+
+box grid_layout::auto_bbox() const
+{
+	// Reset baseline sets.
+	for (size_type x=0;x!=_xcells;++x) _xbs[x]=xbaseline_set();
+	for (size_type y=0;y!=_ycells;++y) _ybs[y]=ybaseline_set();
+
+	// Request min_bbox for each cell.  Incorporate into appropriate
+	// x-baseline and y-baseline set.
+	vector<component*>::const_iterator i=_components.begin();
+	for (size_type y=0;y!=_ycells;++y)
+	{
+		for (size_type x=0;x!=_xcells;++x)
+		{
+			if (component* c=*i++)
+			{
+				box mcbbox=c->min_bbox();
+				_xbs[x].add(mcbbox,c->xbaseline());
+				_ybs[y].add(mcbbox,c->ybaseline());
+			}
+		}
+	}
+
+	// Calculate total width and height.
+	int xsize=0;
+	int ysize=0;
+	for (size_type x=0;x!=_xcells;++x)
+		xsize+=_xbs[x].xsize();
+	for (size_type y=0;y!=_ycells;++y)
+		ysize+=_ybs[y].ysize();
+
+	// Add gaps to width and height.
+	if (_xcells) xsize+=(_xcells-1)*_xgap;
+	if (_ycells) ysize+=(_ycells-1)*_ygap;
+
+	// Add margin to width and height
+	xsize+=_margin.xsize();
+	ysize+=_margin.ysize();
+
+	// Construct minimum bounding box, with respect to top left-hand
+	// corner of layout.
+	box abbox(0,-ysize,xsize,0);
+
+	// Translate to external origin and return.
+	abbox-=external_origin(abbox,xbaseline_left,ybaseline_top);
+	return abbox;
+}
+
+component* grid_layout::find(const point& p) const
+{
+	vector<int>::const_iterator xf=
+		lower_bound(_xmin.begin(),_xmin.end(),p.x()+1,less<int>());
+	vector<int>::const_iterator yf=
+		lower_bound(_ymax.begin(),_ymax.end(),p.y()-1,greater<int>());
+	if (xf==_xmin.begin()) return 0;
+	if (yf==_ymax.begin()) return 0;
+	size_type x=(xf-_xmin.begin())-1;
+	size_type y=(yf-_ymax.begin())-1;
+	component* c=_components[y*_xcells+x];
+	if (!c) return 0;
+	box cbbox=c->bbox()+c->origin();
+	if (!(p<=cbbox)) return 0;
+	return c;
+}
+
+box grid_layout::bbox() const
+{
+	return _bbox;
+}
+
+void grid_layout::resize() const
+{
+	for (vector<component*>::const_iterator i=_components.begin();
+		i!=_components.end();++i)
+	{
+		if (component* c=*i) c->resize();
+	}
+	inherited::resize();
+}
+
+void grid_layout::reformat(const point& origin,const box &bbox)
+{
+	// Update origin and bounding box of this component, force redraw
+	// if necessary.  (This must happen before reformat() is called for
+	// any children.)
+	bool moved=(origin!=this->origin())||(bbox!=this->bbox());
+	if (moved) force_redraw();
+	_bbox=bbox;
+	inherited::reformat(origin,bbox);
+	if (moved) force_redraw();
+
+	// Remove margin.
+	box ibox(_bbox-_margin);
+
+	// Get bounding box before adding excess.
+	box abbox=auto_bbox();
+
+	// Calculate excess in x-direction.
+	int xexcess=_bbox.xsize()-abbox.xsize();
+	if (xexcess<0) xexcess=0;
+	int xspread=abbox.xsize();
+	xspread-=_margin.xsize();
+	if (_xcells) xspread-=(_xcells-1)*_xgap;
+	if (xspread<0) xspread=0;
+	divider xdiv(xexcess,xspread);
+
+	// Set minimum x-coordinate for each column.
+	int xpos=ibox.xmin();
+	_xmin[0]=xpos;
+	for (size_type x=0;x!=_xcells;++x)
+	{
+		int xsize=_xbs[x].xsize();
+		xpos+=xsize+xdiv(xsize);
+		xpos+=_xgap;
+		_xmin[x+1]=xpos;
+	}
+
+	// Calculate excess in y-direction.
+	int yexcess=_bbox.ysize()-abbox.ysize();
+	if (yexcess<0) yexcess=0;
+	int yspread=abbox.ysize();
+	yspread-=_margin.ysize();
+	if (_ycells) yspread-=(_ycells-1)*_ygap;
+	if (yspread<0) yspread=0;
+	divider ydiv(yexcess,yspread);
+
+	// Set minimum and maximum y-coordinates for each row.
+	int ypos=ibox.ymax();
+	_ymax[0]=ypos;
+	for (size_type y=0;y!=_ycells;++y)
+	{
+		int ysize=_ybs[y].ysize();
+		ypos-=ysize+ydiv(ysize);
+		ypos-=_ygap;
+		_ymax[y+1]=ypos;
+	}
+
+	// Place children.
+	vector<component*>::iterator i=_components.begin();
+	for (size_type y=0;y!=_ycells;++y)
+	{
+		for (size_type x=0;x!=_xcells;++x)
+		{
+			if (component* c=*i++)
+			{
+				// Construct bounding box for cell with respect to
+				// origin of layout.
+				box cbbox(_xmin[x],_ymax[y+1]+_ygap,_xmin[x+1]-_xgap,_ymax[y]);
+
+				// Calculate offset from top left-hand corner of cell
+				// to origin of child.
+				int xoffset=_xbs[x].offset(xbaseline_left,c->xbaseline(),
+					cbbox.xsize());
+				int yoffset=_ybs[y].offset(ybaseline_bottom,c->ybaseline(),
+					cbbox.ysize());
+				point coffset(xoffset,yoffset);
+
+				// Calculate origin of cell with respect to origin of
+				// layout.
+				point cpos(cbbox.xminymin()+coffset);
+
+				// Reformat child.
+				c->reformat(cpos,cbbox-cpos);
+			}
+		}
+	}
+}
+
+void grid_layout::unformat()
+{
+	for (vector<component*>::iterator i=_components.begin();
+		i!=_components.end();++i)
+	{
+		if (component* c=*i) c->unformat();
+	}
+}
+
+void grid_layout::redraw(gcontext& context,const box& clip)
+{
+	vector<component*>::iterator i=_components.begin();
+	for (size_type y=0;y!=_ycells;++y)
+	{
+		for (size_type x=0;x!=_xcells;++x)
+		{
+			if (component* c=*i++)
+			{
+				point cpos(c->origin());
+				context+=cpos;
+				c->redraw(context,clip+cpos);
+				context-=cpos;
+			}
+		}
+	}
+	inherited::redraw(context,clip);
+}
+
+void grid_layout::remove_notify(component& c)
+{
+	vector<component*>::iterator f=
+		std::find(_components.begin(),_components.end(),&c);
+	if (f!=_components.end())
+	{
+		*f=0;
+		invalidate();
+	}
+}
+
+grid_layout& grid_layout::cells(size_type xcells,size_type ycells)
+{
+	// Create new child array.
+	vector<component*> components(xcells*ycells);
+
+	// Initialise source and destination iterators.
+	vector<component*>::iterator src=_components.begin();
+	vector<component*>::iterator dst=components.begin();
+
+	// Calculate number of columns and number of rows to be copied
+	// from source to destination.
+	size_type xcopy=min(_xcells,xcells);
+	size_type ycopy=min(_ycells,ycells);
+
+	// Handle rows that exist in source and destination.
+	for (size_type y=0;y!=ycopy;++y)
+	{
+		// Handle columns that exist in source and destination (copy).
+		for (size_type x=0;x!=xcopy;++x)
+			*dst++=*src++;
+		// Handle columns that do not exist in destination (discard).
+		for (size_type x=xcopy;x!=_xcells;++x)
+			if (component* c=*src++) c->remove();
+		// Handle columns that do not exist in source (zero).
+		for (size_type x=xcopy;x!=xcells;++x)
+			*dst++=0;
+	}
+	// Handle rows that do not exist in destination (discard).
+	for (size_type y=ycopy;y!=_ycells;++y)
+	{
+		for (size_type x=0;x!=_xcells;++x)
+			if (component* c=*src++) c->remove();
+	}
+	// Handle rows that do not exist in source (zero).
+	for (size_type y=ycopy;y!=ycells;++y)
+	{
+		for (size_type x=0;x!=xcells;++x)
+			*dst++=0;
+	}
+
+	// Replace existing child array.
+	_xcells=xcells;
+	_ycells=ycells;
+	_components.swap(components);
+
+	// Resize auxiliary data structures.
+	_xmin.resize(xcells+1,0);
+	_ymax.resize(ycells+1,0);
+	_xbs.resize(xcells);
+	_ybs.resize(ycells);
+
+	// Invalidate component and return.
+	invalidate();
+	return *this;
+}
+
+grid_layout& grid_layout::add(component& c,size_type x,size_type y)
+{
+	// If cell does not exist then resize grid.
+	if ((x>=_xcells)||(y>=_ycells))
+		cells(max(_xcells,x+1),max(_ycells,y+1));
+
+	// If cell is occupied then remove content.
+	if (component* c=_components[y*_xcells+x])
+		c->remove();
+
+	// Link from parent to child and child to parent. 
+	_components[y*_xcells+x]=&c;
+	link_child(c);
+
+	// Invalidate component and return.
+	invalidate();
+	return *this;
+}
+
+grid_layout& grid_layout::xgap(int xgap)
+{
+	_xgap=xgap;
+	invalidate();
+	return *this;
+}
+
+grid_layout& grid_layout::ygap(int ygap)
+{
+	_ygap=ygap;
+	invalidate();
+	return *this;
+}
+
+grid_layout& grid_layout::margin(const box& margin)
+{
+	_margin=margin;
+	invalidate();
+	return *this;
+}
+
+grid_layout& grid_layout::margin(int margin)
+{
+	_margin=box(-margin,-margin,margin,margin);
+	invalidate();
+	return *this;
+}
+
+}; /* namespace desktop */
+}; /* namespace rtk */
---
+++ !RTK/rtk/desktop/grid_layout.h
@@ -0,0 +1,176 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#ifndef _RTK_DESKTOP_GRID_LAYOUT
+#define _RTK_DESKTOP_GRID_LAYOUT
+
+#include <vector>
+
+#include "rtk/desktop/sizeable_component.h"
+
+namespace rtk {
+namespace desktop {
+
+/** A layout class for arranging components in a grid.
+ * It is possible to specify the horizontal or vertical gap to be placed
+ * between cells and the margin to be placed around the layout as a whole.
+ * Cells that share the same horizontal or vertical baseline are aligned
+ * with each other. The layout will expand to fill any extra space made
+ * available to it. Note that the algorithm used to do this is
+ * implementation-dependent, so may change in future versions of this
+ * library.
+ * @todo The redraw() function should only redraw cells that lie within
+ *  the clip rectangle, whereas at present it redraws everthing.
+ */
+class grid_layout:
+	public sizeable_component
+{
+private:
+	/** The class from which this one is derived. */
+	typedef sizeable_component inherited;
+public:
+	/** A type for representing cell counts or indices. */
+	typedef unsigned int size_type;
+private:
+	/** The number of columns. */
+	size_type _xcells;
+
+	/** The number of rows. */
+	size_type _ycells;
+
+	/** Vector containing child pointer for each cell. */
+	vector<component*> _components;
+
+	/** Vector containing position of left edge of each cell with
+	 * respect to origin of layout.  The position of the right edge
+	 * is obtained by subtracting _xgap from the left edge of the
+	 * following cell.  A hypothetical value for _xmin[xcells()] is
+	 * included in the vector so that the position of the right edge
+	 * of the last cell can be determined. */
+	vector<int> _xmin;
+
+	/** Vector containing position of top edge of each cell with
+	 * respect to origin of layout.  The position of the bottom edge
+	 * is obtained by adding _ygap from the top edge of the
+	 * following cell.  A hypothetical value for _ymax[ycells()] is
+	 * included in the vector so that the position of the bottom edge
+	 * of the last cell can be determined. */
+	vector<int> _ymax;
+
+	/** Vector containing cached x-baseline set for each cell. */
+	mutable vector<xbaseline_set> _xbs;
+
+	/** Vector containing cached y-baseline set for each cell. */
+	mutable vector<ybaseline_set> _ybs;
+
+	/** The size of gap to be placed between columns. */
+	int _xgap;
+
+	/** The size of gap to be placed between rows. */
+	int _ygap;
+
+	/** The margin to be placed around the whole layout. */
+	box _margin;
+
+	/** The current bounding box. */
+	box _bbox;
+public:
+	/** Construct grid layout.
+	 * @param xcells the required number of columns (defaults to 0)
+	 * @param ycells the required number of rows (defaults to 0)
+	 */
+	grid_layout(size_type xcells=0,size_type ycells=0);
+
+	/** Destroy grid layout. */
+	virtual ~grid_layout();
+
+	virtual box auto_bbox() const;
+	virtual component* find(const point& p) const;
+	virtual box bbox() const;
+	virtual void resize() const;
+	virtual void reformat(const point& origin,const box& bbox);
+	virtual void unformat();
+	virtual void redraw(gcontext& context,const box& clip);
+protected:
+	virtual void remove_notify(component& c);
+public:
+	/** Get number of columns.
+	 * @return the number of columns
+	 */
+	size_type xcells() const
+		{ return _xcells; }
+
+	/** Get number of rows.
+	 * @return the number of rows
+	 */
+	size_type ycells() const
+		{ return _ycells; }
+
+	/** Set number of columns and rows.
+	 * @param xcells the required number columns
+	 * @param ycells the required number rows
+	 * @return a reference to this
+	 */
+	grid_layout& cells(size_type xcells,size_type ycells);
+
+	/** Add component to layout.
+	 * If a component already exists at the specified position
+	 * then it is replaced.
+	 * @param c the component to be added
+	 * @param x the column index
+	 * @param y the row index
+	 * @return a reference to this
+	 */
+	grid_layout& add(component& c,size_type x,size_type y);
+
+	/** Get size of gap between columns.
+	 * @return the size of gap between columns
+	 */
+	int xgap() const
+		{ return _xgap; }
+
+	/** Get size of gap between rows.
+	 * @return the size of gap between rows
+	 */
+	int ygap() const
+		{ return _ygap; }
+
+	/** Set size of gap between columns.
+	 * @param xgap the required gap between columns
+	 * @return a reference to this
+	 */
+	grid_layout& xgap(int xgap);
+
+	/** Set size of gap between rows.
+	 * @param ygap the required gap between rows
+	 * @return a reference to this
+	 */
+	grid_layout& ygap(int ygap);
+
+	/** Get margin around layout.
+	 * @return a box indicating the margin width for each side of the layout
+	 */
+	const box& margin() const
+		{ return _margin; }
+
+	/** Set margin around layout.
+	 * @param margin a box specifying the required margin width for each
+	 *  side of the layout
+	 * @return a reference to this
+	 */
+	grid_layout& margin(const box& margin);
+
+	/** Set margin around layout.
+	 * @param margin an integer specifying the required margin width for
+	 *  all sides of the layout
+	 * @return a reference to this
+	 */
+	grid_layout& margin(int margin);
+};
+
+}; /* namespace desktop */
+}; /* namespace rtk */
+
+#endif
