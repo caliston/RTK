c86bf1175398aa8c729082445641d45cbb65b8d8
gdshaw <gdshaw@fa0f773a-f822-0410-a9df-e7ef4170f6f5>
2003-09-04 20:17:30 +0000

    Added class rtk::desktop::absolute_layout.
    git-svn-id: http://source.riscpkg.org/svn/RTK/trunk@91 fa0f773a-f822-0410-a9df-e7ef4170f6f5

---
+++ !RTK/rtk/desktop/absolute_layout.cc
@@ -0,0 +1,195 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#include <algorithm>
+
+#include "rtk/graphics/gcontext.h"
+#include "rtk/desktop/absolute_layout.h"
+
+namespace rtk {
+namespace desktop {
+
+absolute_layout::absolute_layout()
+{
+	xbaseline(xbaseline_left);
+	ybaseline(ybaseline_top);
+}
+
+absolute_layout::~absolute_layout()
+{
+	while (_components.size())
+		_components.back()->remove();
+}
+
+box absolute_layout::auto_bbox() const
+{
+	box abbox;
+	vector<component*>::const_iterator i=_components.begin();
+	if (i!=_components.end())
+	{
+		component* c=*i++;
+		abbox=(c->min_bbox()+c->origin());
+	}
+	while (i!=_components.end())
+	{
+		component* c=*i++;
+		abbox|=(c->min_bbox()+c->origin());
+	}
+
+	// Add margin.
+	abbox|=_margin;
+
+	// Ensure that bounding box is compatible with baselines.
+	switch (xbaseline())
+	{
+	case xbaseline_left:
+		abbox.xmin(0);
+		break;
+	case xbaseline_right:
+		abbox.xmax(0);
+		break;
+	case xbaseline_centre:
+		{
+			int xsize=max(-abbox.xmin(),abbox.xmax());
+			abbox.xmin(-xsize);
+			abbox.xmax(xsize);
+		}
+		break;
+	default:
+		/* no action. */
+		break;
+	}
+	switch (ybaseline())
+	{
+	case ybaseline_top:
+		abbox.ymax(0);
+		break;
+	case ybaseline_bottom:
+		abbox.ymin(0);
+		break;
+	case ybaseline_centre:
+		{
+			int ysize=max(-abbox.ymin(),abbox.ymax());
+			abbox.ymin(-ysize);
+			abbox.ymax(ysize);
+		}
+		break;
+	default:
+		/* no action. */
+		break;
+	}
+
+	return abbox;
+}
+
+component* absolute_layout::find(const point& p) const
+{
+	for (vector<component*>::const_iterator i=_components.begin();
+		i!=_components.end();++i)
+	{
+		component* c=*i;
+		if (p<=c->bbox()+c->origin()) return c;
+	}
+	return 0;
+}
+
+box absolute_layout::bbox() const
+{
+	return _bbox;
+}
+
+void absolute_layout::resize() const
+{
+	for (vector<component*>::const_iterator i=_components.begin();
+		i!=_components.end();++i)
+	{
+		component* c=*i;
+		c->resize();
+	}
+	inherited::resize();
+}
+
+void absolute_layout::reformat(const point& origin,const box &bbox)
+{
+	// Update origin and bounding box of this component, force redraw
+	// if necessary.  (This must happen before reformat() is called for
+	// any children.)
+	bool moved=(origin!=this->origin())||(bbox!=this->bbox());
+	if (moved) force_redraw();
+	_bbox=bbox;
+	inherited::reformat(origin,bbox);
+	if (moved) force_redraw();
+
+	// Place children.
+	for (vector<component*>::const_iterator i=_components.begin();
+		i!=_components.end();++i)
+	{
+		component* c=*i;
+		c->reformat(c->origin(),c->min_bbox());
+	}
+}
+
+void absolute_layout::unformat()
+{
+	for (vector<component*>::iterator i=_components.begin();
+		i!=_components.end();++i)
+	{
+		component* c=*i;
+		c->unformat();
+	}
+}
+
+void absolute_layout::redraw(gcontext& context,const box& clip)
+{
+	for (vector<component*>::iterator i=_components.begin();
+		i!=_components.end();++i)
+	{
+		if (component* c=*i)
+		{
+			point cpos=c->origin();
+			context+=cpos;
+			c->redraw(context,clip+cpos);
+			context-=cpos;
+		}
+	}
+	inherited::redraw(context,clip);
+}
+
+void absolute_layout::remove_notify(component& c)
+{
+	vector<component*>::iterator f=
+		std::find(_components.begin(),_components.end(),&c);
+	if (f!=_components.end())
+	{
+		_components.erase(f);
+		invalidate();
+	}
+}
+
+absolute_layout& absolute_layout::add(component& c,const point& p)
+{
+	c.origin(p);
+	_components.push_back(&c);
+	link_child(c);
+	invalidate();
+	return *this;
+}
+
+absolute_layout& absolute_layout::margin(const box& margin)
+{
+	_margin=margin;
+	invalidate();
+	return *this;
+}
+
+absolute_layout& absolute_layout::margin(int margin)
+{
+	_margin=box(-margin,-margin,margin,margin);
+	invalidate();
+	return *this;
+}
+
+}; /* namespace desktop */
+}; /* namespace rtk */
---
+++ !RTK/rtk/desktop/absolute_layout.h
@@ -0,0 +1,87 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#ifndef _RTK_DESKTOP_ABSOLUTE_LAYOUT
+#define _RTK_DESKTOP_ABSOLUTE_LAYOUT
+
+#include <vector>
+
+#include "rtk/desktop/sizeable_component.h"
+
+namespace rtk {
+namespace desktop {
+
+/** A layout class for arranging components by specifying their coordinates.
+ * This class is provided for use when the required layout does not have a
+ * logical structure, or where the structure is too complex to be represented
+ * using other layout classes (such as rows, columns and grids).
+ */
+class absolute_layout:
+	public sizeable_component
+{
+private:
+	/** The class from which this one is derived. */
+	typedef sizeable_component inherited;
+
+	/** A vector containing a pointer to each child. */
+	vector<component*> _components;
+
+	/** The margin to be placed around the whole layout. */
+	box _margin;
+
+	/** The current bounding box. */
+	box _bbox;
+public:
+	/** Construct absolute layout.
+	 * By default, the origin of an absolute layout is in the top
+	 * left-hand corner.
+	 */
+	absolute_layout();
+
+	/** Destroy absolute layout. */
+	virtual ~absolute_layout();
+
+	virtual box auto_bbox() const;
+	virtual component* find(const point& p) const;
+	virtual box bbox() const;
+	virtual void resize() const;
+	virtual void reformat(const point& origin,const box &bbox);
+	virtual void unformat();
+	virtual void redraw(gcontext& context,const box& clip);
+protected:
+	virtual void remove_notify(component& c);
+public:
+	/** Add component to layout.
+	 * @param c the component to be added
+	 * @param p the required initial origin
+	 * @return a reference to this
+	 */
+	absolute_layout& add(component& c,const point& p);
+
+	/** Get margin around layout.
+	 * @return a box indicating the margin width for each side of the layout
+	 */
+	const box& margin() const
+		{ return _margin; }
+
+	/** Set margin around layout.
+	 * @param margin a box specifying the required margin width for each
+	 *  side of the layout
+	 * @return a reference to this
+	 */
+	absolute_layout& margin(const box& margin);
+
+	/** Set margin around layout.
+	 * @param margin an integer specifying the required margin width for
+	 *  all sides of the layout
+	 * @return a reference to this
+	 */
+	absolute_layout& margin(int margin);
+};
+
+}; /* namespace desktop */
+}; /* namespace rtk */
+
+#endif
