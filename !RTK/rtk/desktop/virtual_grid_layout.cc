c6e4f509f25babafd31e6156fed4dddfaf05fafc
gdshaw <gdshaw@fa0f773a-f822-0410-a9df-e7ef4170f6f5>
2003-09-07 05:50:40 +0000

    Added class rtk::desktop::virtual_grid_layout.
    git-svn-id: http://source.riscpkg.org/svn/RTK/trunk@96 fa0f773a-f822-0410-a9df-e7ef4170f6f5

---
+++ !RTK/rtk/desktop/virtual_grid_layout.cc
@@ -0,0 +1,311 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#include <algorithm>
+#include <functional>
+
+#include "rtk/util/divider.h"
+#include "rtk/graphics/gcontext.h"
+#include "rtk/desktop/virtual_grid_layout.h"
+
+namespace rtk {
+namespace desktop {
+
+using rtk::util::divider;
+
+virtual_grid_layout::virtual_grid_layout(size_type xcells,size_type ycells):
+	_xcells(xcells),
+	_ycells(ycells),
+	_xmin(xcells+1,0),
+	_ymax(ycells+1,0),
+	_xbs(xcells),
+	_ybs(ycells),
+	_xgap(0),
+	_ygap(0)
+{}
+
+virtual_grid_layout::~virtual_grid_layout()
+{}
+
+box virtual_grid_layout::auto_bbox() const
+{
+	// Reset baseline sets.
+	for (size_type x=0;x!=_xcells;++x) _xbs[x]=xbaseline_set();
+	for (size_type y=0;y!=_ycells;++y) _ybs[y]=ybaseline_set();
+
+	// Request min_bbox for each cell.  Incorporate into appropriate
+	// x-baseline and y-baseline set.
+	for (size_type y=0;y!=_ycells;++y)
+	{
+		for (size_type x=0;x!=_xcells;++x)
+		{
+			box mcbbox=cell_min_bbox(x,y);
+			_xbs[x].add(mcbbox,cell_xbaseline(x,y));
+			_ybs[y].add(mcbbox,cell_ybaseline(x,y));
+		}
+	}
+
+	// Calculate total width and height.
+	int xsize=0;
+	int ysize=0;
+	for (size_type x=0;x!=_xcells;++x)
+		xsize+=_xbs[x].xsize();
+	for (size_type y=0;y!=_ycells;++y)
+		ysize+=_ybs[y].ysize();
+
+	// Add gaps to width and height.
+	if (_xcells) xsize+=(_xcells-1)*_xgap;
+	if (_ycells) ysize+=(_ycells-1)*_ygap;
+
+	// Add margin to width and height
+	xsize+=_margin.xsize();
+	ysize+=_margin.ysize();
+
+	// Construct minimum bounding box, with respect to top left-hand
+	// corner of layout.
+	box abbox(0,-ysize,xsize,0);
+
+	// Translate to external origin and return.
+	abbox-=external_origin(abbox,xbaseline_left,ybaseline_top);
+	return abbox;
+}
+
+box virtual_grid_layout::bbox() const
+{
+	return _bbox;
+}
+
+void virtual_grid_layout::reformat(const point& origin,const box &bbox)
+{
+	// Update origin and bounding box of this component, force redraw
+	// if necessary.  (This must happen before reformat() is called for
+	// any children.)
+	bool moved=(origin!=this->origin())||(bbox!=this->bbox());
+	if (moved) force_redraw();
+	_bbox=bbox;
+	inherited::reformat(origin,bbox);
+	if (moved) force_redraw();
+
+	// Remove margin.
+	box ibox(_bbox-_margin);
+
+	// Get bounding box before adding excess.
+	box abbox=auto_bbox();
+
+	// Calculate excess in x-direction.
+	int xexcess=_bbox.xsize()-abbox.xsize();
+	if (xexcess<0) xexcess=0;
+	int xspread=abbox.xsize();
+	xspread-=_margin.xsize();
+	if (_xcells) xspread-=(_xcells-1)*_xgap;
+	if (xspread<0) xspread=0;
+	divider xdiv(xexcess,xspread);
+
+	// Set minimum x-coordinate for each column.
+	int xpos=ibox.xmin();
+	_xmin[0]=xpos;
+	for (size_type x=0;x!=_xcells;++x)
+	{
+		int xsize=_xbs[x].xsize();
+		xpos+=xsize+xdiv(xsize);
+		xpos+=_xgap;
+		_xmin[x+1]=xpos;
+	}
+
+	// Calculate excess in y-direction.
+	int yexcess=_bbox.ysize()-abbox.ysize();
+	if (yexcess<0) yexcess=0;
+	int yspread=abbox.ysize();
+	yspread-=_margin.ysize();
+	if (_ycells) yspread-=(_ycells-1)*_ygap;
+	if (yspread<0) yspread=0;
+	divider ydiv(yexcess,yspread);
+
+	// Set minimum and maximum y-coordinates for each row.
+	int ypos=ibox.ymax();
+	_ymax[0]=ypos;
+	for (size_type y=0;y!=_ycells;++y)
+	{
+		int ysize=_ybs[y].ysize();
+		ypos-=ysize+ydiv(ysize);
+		ypos-=_ygap;
+		_ymax[y+1]=ypos;
+	}
+
+	// Place children.
+	for (size_type y=0;y!=_ycells;++y)
+	{
+		for (size_type x=0;x!=_xcells;++x)
+		{
+			// Construct bounding box for cell with respect to
+			// origin of layout.
+			box cbbox(_xmin[x],_ymax[y+1]+_ygap,_xmin[x+1]-_xgap,_ymax[y]);
+
+			// Calculate offset from top left-hand corner of cell
+			// to origin of child.
+			int xoffset=_xbs[x].offset(xbaseline_left,cell_xbaseline(x,y),
+				cbbox.xsize());
+			int yoffset=_ybs[y].offset(ybaseline_bottom,cell_ybaseline(x,y),
+				cbbox.ysize());
+			point coffset(xoffset,yoffset);
+
+			// Calculate origin of cell with respect to origin of
+			// layout.
+			point cpos(cbbox.xminymin()+coffset);
+
+			// Reformat child.
+			cell_reformat(x,y,cpos,cbbox-cpos);
+		}
+	}
+}
+
+void virtual_grid_layout::redraw(gcontext& context,const box& clip)
+{
+	// Look for the first column with a right edge which overlaps (or is to
+	// the right of) the clip box: _xmin[x0+1] -_xgap > clip.xmin().
+	vector<int>::iterator xf0=upper_bound(
+		_xmin.begin(),_xmin.end(),clip.xmin()+_xgap,less<int>());
+	size_type x0=xf0-_xmin.begin();
+	if (x0) --x0;
+
+	// Look for the first column with a left edge which is to the right of
+	// the clip box: _xmin[x1] >= clip.xmax().
+	vector<int>::iterator xf1=lower_bound(
+		_xmin.begin(),_xmin.end(),clip.xmax(),less<int>());
+	size_type x1=xf1-_xmin.begin();
+	if (x1>_xcells) x1=_xcells;
+
+	// Look for the first row with a lower edge which overlaps (or is
+	// below) the clip box: _ymax[y0+1] + _ygap < clip.ymax().
+	vector<int>::iterator yf0=upper_bound(
+		_ymax.begin(),_ymax.end(),clip.ymax()-_ygap,greater<int>());
+	size_type y0=yf0-_ymax.begin();
+	if (y0) --y0;
+
+	// Look for the first row with an upper edge which is below the
+	// clip box: _ymax[y1] <= clip.ymin().
+	vector<int>::iterator yf1=lower_bound(
+		_ymax.begin(),_ymax.end(),clip.ymin(),greater<int>());
+	size_type y1=yf1-_ymax.begin();
+	if (y1>_ycells) y1=_ycells;
+
+	// Redraw children.
+	// For safety, use inequalities in the for-loops.
+	for (size_type y=y0;y<y1;++y)
+	{
+		for (size_type x=x0;x<x1;++x)
+		{
+			// Construct bounding box for cell with respect to
+			// origin of layout.
+			box cbbox(_xmin[x],_ymax[y+1]+_ygap,_xmin[x+1]-_xgap,_ymax[y]);
+
+			// Calculate offset from top left-hand corner of cell
+			// to origin of child.
+			int xoffset=_xbs[x].offset(xbaseline_left,cell_xbaseline(x,y),
+				cbbox.xsize());
+			int yoffset=_ybs[y].offset(ybaseline_bottom,cell_ybaseline(x,y),
+				cbbox.ysize());
+			point coffset(xoffset,yoffset);
+
+			// Calculate origin of cell with respect to origin of
+			// layout.
+			point cpos(cbbox.xminymin()+coffset);
+
+			// Redraw cell.
+			context+=cpos;
+			cell_redraw(x,y,context,clip+cpos);
+			context-=cpos;
+		}
+	}
+	inherited::redraw(context,clip);
+}
+
+virtual_grid_layout& virtual_grid_layout::cells(size_type xcells,
+	size_type ycells)
+{
+	_xcells=xcells;
+	_ycells=ycells;
+	_xmin.resize(xcells+1,0);
+	_ymax.resize(ycells+1,0);
+	_xbs.resize(xcells);
+	_ybs.resize(ycells);
+	invalidate();
+	return *this;
+}
+
+virtual_grid_layout& virtual_grid_layout::xgap(int xgap)
+{
+	_xgap=xgap;
+	invalidate();
+	return *this;
+}
+
+virtual_grid_layout& virtual_grid_layout::ygap(int ygap)
+{
+	_ygap=ygap;
+	invalidate();
+	return *this;
+}
+
+virtual_grid_layout& virtual_grid_layout::margin(const box& margin)
+{
+	_margin=margin;
+	invalidate();
+	return *this;
+}
+
+virtual_grid_layout& virtual_grid_layout::margin(int margin)
+{
+	_margin=box(-margin,-margin,margin,margin);
+	invalidate();
+	return *this;
+}
+
+virtual_grid_layout::xbaseline_type virtual_grid_layout::cell_xbaseline(
+	size_type x,size_type y) const
+{
+	return xbaseline_text;
+}
+
+virtual_grid_layout::ybaseline_type virtual_grid_layout::cell_ybaseline(
+	size_type x,size_type y) const
+{
+	return ybaseline_text;
+}
+
+void virtual_grid_layout::cell_reformat(size_type x,size_type y,
+	const point& origin,const box &bbox)
+{}
+
+virtual_grid_layout::size_type virtual_grid_layout::find_x(const point& p)
+{
+	// Look for the first cell with a left edge which is to the right
+	// of the point: _xmin[x] > p.x().
+	vector<int>::const_iterator xf=
+		upper_bound(_xmin.begin(),_xmin.end(),p.x(),less<int>());
+	if (xf==_xmin.begin()) return npos;
+	size_type x=(xf-_xmin.begin())-1;
+
+	// Return npos if point outside cell limits, otherwise return y.
+	if ((p.x()<_xmin[x])||(p.x()>=_xmin[x+1]-_xgap)) return npos;
+	return x;
+}
+
+virtual_grid_layout::size_type virtual_grid_layout::find_y(const point& p)
+{
+	// Look for the first cell with a top edge which is below the point:
+	// _ymax[x] <= p.x().
+	vector<int>::const_iterator yf=
+		lower_bound(_ymax.begin(),_ymax.end(),p.y(),greater<int>());
+	if (yf==_ymax.begin()) return npos;
+	size_type y=(yf-_ymax.begin())-1;
+
+	// Return npos if point outside cell limits, otherwise return y.
+	if ((p.y()>=_ymax[y])||(p.y()<_ymax[y+1]+_ygap)) return npos;
+	return y;
+}
+
+}; /* namespace desktop */
+}; /* namespace rtk */
---
+++ !RTK/rtk/desktop/virtual_grid_layout.h
@@ -0,0 +1,222 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#ifndef _RTK_DESKTOP_VIRTUAL_GRID_LAYOUT
+#define _RTK_DESKTOP_VIRTUAL_GRID_LAYOUT
+
+#include <vector>
+
+#include "rtk/desktop/sizeable_component.h"
+
+namespace rtk {
+namespace desktop {
+
+/** A layout class for arranging components in a grid.
+ * This class produces the same visual appearance as a grid_layout,
+ * without the need for the members of the row to exist as components.
+ */
+class virtual_grid_layout:
+	public sizeable_component
+{
+private:
+	/** The class from which this one is derived. */
+	typedef sizeable_component inherited;
+public:
+	/** A type for representing cell counts or indices. */
+	typedef unsigned int size_type;
+
+	/** A null value for use in place of a cell index. */
+	static const size_type npos=static_cast<size_type>(-1);
+private:
+	/** The number of columns. */
+	size_type _xcells;
+
+	/** The number of rows. */
+	size_type _ycells;
+
+	/** A vector containing the position of the left edge of each cell
+	 * with respect to the origin of the layout.  The position of the
+	 * right edge is obtained by subtracting _xgap from the left edge of
+	 * the following cell.  A hypothetical value for _xmin[xcells()] is
+	 * included in the vector so that the position of the right edge
+	 * of the last cell can be determined. */
+	vector<int> _xmin;
+
+	/** A vector containing the position of the top edge of each cell
+	 * with respect to the origin of the layout.  The position of the
+	 * bottom edge is obtained by adding _ygap from the top edge of
+	 * the following cell.  A hypothetical value for _ymax[ycells()] is
+	 * included in the vector so that the position of the bottom edge
+	 * of the last cell can be determined. */
+	vector<int> _ymax;
+
+	/** A vector containing a cached x-baseline set for each column. */
+	mutable vector<xbaseline_set> _xbs;
+
+	/** A vector containing a cached y-baseline set for each row. */
+	mutable vector<ybaseline_set> _ybs;
+
+	/** The size of gap to be placed between columns. */
+	int _xgap;
+
+	/** The size of gap to be placed between rows. */
+	int _ygap;
+
+	/** The margin to be placed around the whole layout. */
+	box _margin;
+
+	/** The current bounding box. */
+	box _bbox;
+public:
+	/** Construct virtual grid layout.
+	 * @param xcells the required number of columns (defaults to 0)
+	 * @param ycells the required number of rows (defaults to 0)
+	 */
+	virtual_grid_layout(size_type xcells=0,size_type ycells=0);
+
+	/** Destroy virtual grid layout. */
+	virtual ~virtual_grid_layout();
+
+	virtual box auto_bbox() const;
+	virtual box bbox() const;
+	virtual void reformat(const point& origin,const box& bbox);
+	virtual void redraw(gcontext& context,const box& clip);
+public:
+	/** Get number of columns.
+	 * @return the number of columns
+	 */
+	size_type xcells() const
+		{ return _xcells; }
+
+	/** Get number of rows.
+	 * @return the number of rows
+	 */
+	size_type ycells() const
+		{ return _ycells; }
+
+	/** Set number of columns and rows.
+	 * @param xcells the required number columns
+	 * @param ycells the required number rows
+	 * @return a reference to this
+	 */
+	virtual_grid_layout& cells(size_type xcells,size_type ycells);
+
+	/** Get size of gap between columns.
+	 * @return the size of gap between columns
+	 */
+	int xgap() const
+		{ return _xgap; }
+
+	/** Get size of gap between rows.
+	 * @return the size of gap between rows
+	 */
+	int ygap() const
+		{ return _ygap; }
+
+	/** Set size of gap between columns.
+	 * @param xgap the required gap between columns
+	 * @return a reference to this
+	 */
+	virtual_grid_layout& xgap(int xgap);
+
+	/** Set size of gap between rows.
+	 * @param ygap the required gap between rows
+	 * @return a reference to this
+	 */
+	virtual_grid_layout& ygap(int ygap);
+
+	/** Get margin around layout.
+	 * @return a box indicating the margin width for each side of the layout
+	 */
+	const box& margin() const
+		{ return _margin; }
+
+	/** Set margin around layout.
+	 * @param margin a box specifying the required margin width for each
+	 *  side of the layout
+	 * @return a reference to this
+	 */
+	virtual_grid_layout& margin(const box& margin);
+
+	/** Set margin around layout.
+	 * @param margin an integer specifying the required margin width for
+	 *  all sides of the layout
+	 * @return a reference to this
+	 */
+	virtual_grid_layout& margin(int margin);
+
+	/** Get minimum bounding box for cell.
+	 * This function is equivalent to get_bbox() but acts on a virtual
+	 * child instead of a real component.
+	 * @param x the x-index of the cell
+	 * @param y the y-index of the cell
+	 */
+	virtual box cell_min_bbox(size_type x,size_type y) const=0;
+
+	/** Get vertical baseline for cell.
+	 * This function is equivalent to xbaseline() but acts on a virtual
+	 * child instead of a real component.  By default it returns
+	 * xbaseline_text.
+	 * @param x the x-index of the cell
+	 * @param y the y-index of the cell
+	 */
+	virtual xbaseline_type cell_xbaseline(size_type x,size_type y) const;
+
+	/** Get horizontal baseline for cell.
+	 * This function is equivalent to ybaseline() but acts on a virtual
+	 * child instead of a real component.  By default it returns
+	 * ybaseline_text.
+	 * @param x the x-index of the cell
+	 * @param y the y-index of the cell
+	 */
+	virtual ybaseline_type cell_ybaseline(size_type x,size_type y) const;
+
+	/** Reformat cell to fit within specified bounding box.
+	 * This function is equivalent to reformat() but acts on a virtual
+	 * child instead of a real component.  By default it does nothing.
+	 * @param x the x-index of the cell
+	 * @param y the y-index of the cell
+	 * @param origin the new origin of this cell, with respect to
+	 *  its parent
+	 * @param bbox the available bounding box for this cell, with
+	 *  respect to its own origin
+	 */
+	virtual void cell_reformat(size_type x,size_type y,const point& origin,
+		const box &bbox);
+
+	/** Redraw call.
+	 * This function is equivalent to redraw() but acts on a virtual
+	 * child instead of a real component.
+	 * @param x the x-index of the cell
+	 * @param y the y-index of the cell
+	 * @param context the graphics context within which the
+	 *  redraw should take place
+	 * @param clip the bounding box of the region to be redrawn,
+	 *  with respect to the origin of the cell
+	 */
+	virtual void cell_redraw(size_type x,size_type y,gcontext& context,
+		const box& clip)=0;
+
+	/** Get x-index of cell containing point.
+	 * @param p the point to find, with respect to the origin of this
+	 *  component.
+	 * @return the x-index of the cell containing p, or npos if there is
+	 *  no such cell.
+	 */
+	size_type find_x(const point& p);
+
+	/** Get y-index of cell containing point.
+	 * @param p the point to find, with respect to the origin of this
+	 *  component.
+	 * @return the y-index of the cell containing p, or npos if there is
+	 *  no such cell.
+	 */
+	size_type find_y(const point& p);
+};
+
+}; /* namespace desktop */
+}; /* namespace rtk */
+
+#endif
