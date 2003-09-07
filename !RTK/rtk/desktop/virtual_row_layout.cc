3ff2a9cb60e035df5a005b3288199323292c007a
gdshaw <gdshaw@fa0f773a-f822-0410-a9df-e7ef4170f6f5>
2003-09-07 05:47:56 +0000

    Added class rtk::desktop::virtual_row_layout.
    git-svn-id: http://source.riscpkg.org/svn/RTK/trunk@94 fa0f773a-f822-0410-a9df-e7ef4170f6f5

---
+++ !RTK/rtk/desktop/virtual_row_layout.cc
@@ -0,0 +1,244 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#include <algorithm>
+#include <functional>
+
+#include "rtk/util/divider.h"
+#include "rtk/graphics/gcontext.h"
+#include "rtk/desktop/virtual_row_layout.h"
+
+namespace rtk {
+namespace desktop {
+
+using rtk::util::divider;
+
+virtual_row_layout::virtual_row_layout(size_type xcells):
+	_xcells(xcells),
+	_xmin(xcells+1,0),
+	_xgap(0)
+{}
+
+virtual_row_layout::~virtual_row_layout()
+{}
+
+box virtual_row_layout::auto_bbox() const
+{
+	// Reset y-baseline set.
+	_ybs=ybaseline_set();
+
+	// Request min_bbox for each cell.  Incorporate into y-baseline set
+	// and total width.
+	int xsize=0;
+	for (size_type x=0;x!=_xcells;++x)
+	{
+		box mcbbox=cell_min_bbox(x);
+		_ybs.add(mcbbox,cell_ybaseline(x));
+		xsize+=mcbbox.xsize();
+	}
+
+	// Calculate combined height.
+	int ysize=_ybs.ysize();
+
+	// Add gaps to total width.
+	if (_xcells) xsize+=(_xcells-1)*_xgap;
+
+	// Add margin to width and height.
+	xsize+=_margin.xsize();
+	ysize+=_margin.ysize();
+
+	// Construct minimum bounding box, with respect to top left-hand
+	// corner of layout.
+	box abbox(0,-ysize,xsize,0);
+
+	// Translate to external origin and return.
+	abbox-=external_origin(abbox,xbaseline_left,ybaseline_top);
+	return abbox;
+}
+
+box virtual_row_layout::bbox() const
+{
+	return _bbox;
+}
+
+void virtual_row_layout::reformat(const point& origin,const box &bbox)
+{
+	// Update origin and bounding box of this component, force redraw
+	// if necessary.  (This must happen before reformat() is called for
+	// any children.)
+	bool moved=(origin!=this->origin())||(bbox!=this->bbox());
+	if (moved) force_redraw();
+	_bbox=bbox;
+	inherited::reformat(origin,bbox);
+	if (moved) force_redraw();
+
+	// Remove margin.
+	box ibox(_bbox-_margin);
+
+	// Get bounding box before adding excess.
+	box abbox=auto_bbox();
+
+	// Calculate excess.
+	int xexcess=_bbox.xsize()-abbox.xsize();
+	if (xexcess<0) xexcess=0;
+	int xspread=abbox.xsize();
+	xspread-=_margin.xsize();
+	if (_xcells) xspread-=(_xcells-1)*_xgap;
+	if (xspread<0) xspread=0;
+	divider xdiv(xexcess,xspread);
+
+	// Set minimum x-coordinate for each cell.
+	int xpos=ibox.xmin();
+	_xmin[0]=xpos;
+	for (size_type x=0;x!=_xcells;++x)
+	{
+		// Get minimum bounding box for cell.
+		box mcbbox=cell_min_bbox(x);
+
+		// Set minimum x-coordinate for next cell.
+		int xsize=mcbbox.xsize();
+		xpos+=xsize+xdiv(xsize);
+		xpos+=_xgap;
+		_xmin[x+1]=xpos;
+	}
+
+	// Place children.
+	for (size_type x=0;x!=_xcells;++x)
+	{
+		// Create x-baseline set for just this cell.
+		xbaseline_set xbs;
+		xbs.add(cell_min_bbox(x),cell_xbaseline(x));
+
+		// Construct bounding box for cell with respect to origin
+		// of layout.
+		box cbbox(_xmin[x],ibox.ymin(),_xmin[x+1]-_xgap,ibox.ymax());
+
+		// Calculate offset from top left-hand corner of cell to
+		// origin of child.
+		int xoffset=xbs.offset(xbaseline_left,cell_xbaseline(x),
+			cbbox.xsize());
+		int yoffset=_ybs.offset(ybaseline_bottom,cell_ybaseline(x),
+			cbbox.ysize());
+		point coffset(xoffset,yoffset);
+
+		// Calculate origin of cell with respect to origin of layout.
+		point cpos(cbbox.xminymin()+coffset);
+
+		// Reformat child.
+		cell_reformat(x,cpos,cbbox-cpos);
+	}
+}
+
+void virtual_row_layout::redraw(gcontext& context,const box& clip)
+{
+	// Remove margin.
+	box ibox(_bbox-_margin);
+
+	// Look for the first column with a right edge which overlaps (or is to
+	// the right of) the clip box: _xmin[x0+1] -_xgap > clip.xmin().
+	vector<int>::iterator xf0=upper_bound(
+		_xmin.begin(),_xmin.end(),clip.xmin()+_xgap,less<int>());
+	size_type x0=xf0-_xmin.begin();
+	if (x0) --x0;
+
+	// Look for the first column with a left edge which is to the right of
+	// the clip box: _xmin[x1] >= clip.xmax().
+	vector<int>::iterator xf1=lower_bound(
+		_xmin.begin(),_xmin.end(),clip.xmax(),less<int>());
+	size_type x1=xf1-_xmin.begin();
+	if (x1>_xcells) x1=_xcells;
+
+	// Redraw children.
+	// For safety, use an inequality in the for-loop.
+	for (size_type x=x0;x<x1;++x)
+	{
+		// Create x-baseline set for just this cell.
+		xbaseline_set xbs;
+		xbs.add(cell_min_bbox(x),cell_xbaseline(x));
+
+		// Construct bounding box for cell with respect to origin
+		// of layout.
+		box cbbox(_xmin[x],ibox.ymin(),_xmin[x+1]-_xgap,ibox.ymax());
+
+		// Calculate offset from top left-hand corner of cell to
+		// origin of child.
+		int xoffset=xbs.offset(xbaseline_left,cell_xbaseline(x),
+			cbbox.xsize());
+		int yoffset=_ybs.offset(ybaseline_bottom,cell_ybaseline(x),
+			cbbox.ysize());
+		point coffset(xoffset,yoffset);
+
+		// Calculate origin of cell with respect to origin of layout.
+		point cpos(cbbox.xminymin()+coffset);
+
+		// Redraw cell.
+		context+=cpos;
+		cell_redraw(x,context,clip+cpos);
+		context-=cpos;
+	}
+	inherited::redraw(context,clip);
+}
+
+virtual_row_layout& virtual_row_layout::cells(size_type xcells)
+{
+	_xcells=xcells;
+	_xmin.resize(xcells+1,0);
+	invalidate();
+	return *this;
+}
+
+virtual_row_layout& virtual_row_layout::xgap(int xgap)
+{
+	_xgap=xgap;
+	invalidate();
+	return *this;
+}
+
+virtual_row_layout& virtual_row_layout::margin(const box& margin)
+{
+	_margin=margin;
+	invalidate();
+	return *this;
+}
+
+virtual_row_layout& virtual_row_layout::margin(int margin)
+{
+	_margin=box(-margin,-margin,margin,margin);
+	invalidate();
+	return *this;
+}
+
+virtual_row_layout::xbaseline_type virtual_row_layout::cell_xbaseline(
+	size_type x) const
+{
+	return xbaseline_text;
+}
+
+virtual_row_layout::ybaseline_type virtual_row_layout::cell_ybaseline(
+	size_type x) const
+{
+	return ybaseline_text;
+}
+
+void virtual_row_layout::cell_reformat(size_type x,const point& origin,
+	const box &bbox)
+{}
+
+virtual_row_layout::size_type virtual_row_layout::find_x(const point& p)
+{
+	// Look for the first cell with a left edge which is to the right
+	// of the point: _xmin[x] > p.x().
+	vector<int>::const_iterator xf=
+		upper_bound(_xmin.begin(),_xmin.end(),p.x(),less<int>());
+	if (xf==_xmin.begin()) return npos;
+	size_type x=(xf-_xmin.begin())-1;
+
+	// Return npos if point outside cell limits, otherwise return y.
+	if ((p.x()<_xmin[x])||(p.x()>=_xmin[x+1]-_xgap)) return npos;
+	return x;
+}
+
+}; /* namespace desktop */
+}; /* namespace rtk */
---
+++ !RTK/rtk/desktop/virtual_row_layout.h
@@ -0,0 +1,171 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#ifndef _RTK_DESKTOP_VIRTUAL_ROW_LAYOUT
+#define _RTK_DESKTOP_VIRTUAL_ROW_LAYOUT
+
+#include <vector>
+
+#include "rtk/desktop/sizeable_component.h"
+
+namespace rtk {
+namespace desktop {
+
+/** A layout class for arranging cells in a row.
+ * This class produces the same visual appearance as a row_layout,
+ * without the need for the members of the row to exist as components.
+ */
+class virtual_row_layout:
+	public sizeable_component
+{
+private:
+	/** The class from which this one is derived. */
+	typedef sizeable_component inherited;
+public:
+	/** A type for representing cell counts or indices. */
+	typedef unsigned int size_type;
+
+	/** A null value for use in place of a cell index. */
+	static const size_type npos=static_cast<size_type>(-1);
+private:
+	/** The number of cells. */
+	size_type _xcells;
+
+	/** A vector containing the position of the left edge of each cell
+	 * with respect to origin of layout.  The position of the right
+	 * edge is obtained by subtracting _xgap from the left edge of the
+	 * following cell.  A hypothetical value for _xmin[xcells()] is
+	 * included in the vector so that the position of the right edge
+	 * of the last cell can be determined. */
+	vector<int> _xmin;
+
+	/** The cached y-baseline set for the layout. */
+	mutable ybaseline_set _ybs;
+
+	/** The size of gap to be placed between cells. */
+	int _xgap;
+
+	/** The margin to be placed around the whole layout. */
+	box _margin;
+
+	/** The current bounding box. */
+	box _bbox;
+public:
+	/** Construct virtual row layout.
+	 * @param xcells the required number of cells (defaults to 0)
+	 */
+	virtual_row_layout(size_type xcells=0);
+
+	/** Destroy virtual row layout. */
+	virtual ~virtual_row_layout();
+
+	virtual box auto_bbox() const;
+	virtual box bbox() const;
+	virtual void reformat(const point& origin,const box &bbox);
+	virtual void redraw(gcontext& context,const box& clip);
+public:
+	/** Get number of cells.
+	 * @return the number of cells
+	 */
+	size_type xcells() const
+		{ return _xcells; }
+
+	/** Set number of cells.
+	 * @param xcells the required number of cells
+	 * @return a reference to this
+	 */
+	virtual_row_layout& cells(size_type xcells);
+
+	/** Get size of gap between cells.
+	 * @return the size of gap between cells
+	 */
+	int xgap() const
+		{ return _xgap; }
+
+	/** Set size of gap between cells.
+	 * @param xgap the required gap between cells
+	 * @return a reference to this
+	 */
+	virtual_row_layout& xgap(int xgap);
+
+	/** Get margin around layout.
+	 * @return a box indicating the margin width for each side of the layout
+	 */
+	box margin() const
+		{ return _margin; }
+
+	/** Set margin around layout.
+	 * @param margin a box specifying the required margin width for each
+	 *  side of the layout
+	 * @return a reference to this
+	 */
+	virtual_row_layout& margin(const box& margin);
+
+	/** Set margin around layout.
+	 * @param margin an integer specifying the required margin width for
+	 *  all sides of the layout
+	 * @return a reference to this
+	 */
+	virtual_row_layout& margin(int margin);
+
+	/** Get minimum bounding box for cell.
+	 * This function is equivalent to get_bbox() but acts on a virtual
+	 * child instead of a real component.
+	 * @param x the x-index of the cell
+	 */
+	virtual box cell_min_bbox(size_type x) const=0;
+
+	/** Get vertical baseline for cell.
+	 * This function is equivalent to xbaseline() but acts on a virtual
+	 * child instead of a real component.  By default it returns
+	 * xbaseline_text.
+	 * @param x the x-index of the cell
+	 */
+	virtual xbaseline_type cell_xbaseline(size_type x) const;
+
+	/** Get horizontal baseline for cell.
+	 * This function is equivalent to ybaseline() but acts on a virtual
+	 * child instead of a real component.  By default it returns
+	 * ybaseline_text.
+	 * @param x the x-index of the cell
+	 */
+	virtual ybaseline_type cell_ybaseline(size_type x) const;
+
+	/** Reformat cell to fit within specified bounding box.
+	 * This function is equivalent to reformat() but acts on a virtual
+	 * child instead of a real component.  By default it does nothing.
+	 * @param x the x-index of the cell
+	 * @param origin the new origin of this cell, with respect to
+	 *  its parent
+	 * @param bbox the available bounding box for this cell, with
+	 *  respect to its own origin
+	 */
+	virtual void cell_reformat(size_type x,const point& origin,
+		const box &bbox);
+
+	/** Redraw call.
+	 * This function is equivalent to redraw() but acts on a virtual
+	 * child instead of a real component.
+	 * @param x the x-index of the cell
+	 * @param context the graphics context within which the
+	 *  redraw should take place
+	 * @param clip the bounding box of the region to be redrawn,
+	 *  with respect to the origin of the cell
+	 */
+	virtual void cell_redraw(size_type x,gcontext& context,const box& clip)=0;
+
+	/** Get x-index of cell containing point.
+	 * @param p the point to find, with respect to the origin of this
+	 *  component.
+	 * @return the x-index of the cell containing p, or npos if there is
+	 *  no such cell.
+	 */
+	size_type find_x(const point& p);
+};
+
+}; /* namespace desktop */
+}; /* namespace rtk */
+
+#endif
