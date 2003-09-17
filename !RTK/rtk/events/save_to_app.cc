c999f713f42c8f921872e113143c07a98dcad297
gdshaw <gdshaw@fa0f773a-f822-0410-a9df-e7ef4170f6f5>
2003-09-17 07:30:13 +0000

    Added class rtk::events::save_to_app.
    git-svn-id: http://source.riscpkg.org/svn/RTK/trunk@123 fa0f773a-f822-0410-a9df-e7ef4170f6f5

---
+++ !RTK/rtk/events/save_to_app.cc
@@ -0,0 +1,46 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#include "rtk/os/wimp.h"
+#include "rtk/desktop/component.h"
+#include "rtk/events/save_to_app.h"
+
+namespace rtk {
+namespace events {
+
+using rtk::desktop::component;
+
+save_to_app::save_to_app(component& target,const string& leafname):
+	event(target),
+	_leafname(leafname)
+{
+	os::pointer_info_get block;
+	os::Wimp_GetPointerInfo(block);
+	_whandle=block.whandle;
+	_ihandle=block.ihandle;
+	_position=block.p;
+};
+
+save_to_app::save_to_app(component& target,int whandle,int ihandle,
+	const point& position,const string& leafname):
+	event(target),
+	_whandle(whandle),
+	_ihandle(ihandle),
+	_position(position),
+	_leafname(leafname)
+{}
+
+save_to_app::~save_to_app()
+{}
+
+bool save_to_app::deliver(component& dest)
+{
+	handler* h=dynamic_cast<handler*>(&dest);
+	if (h) h->handle_event(*this);
+	return h;
+}
+
+}; /* namespace events */
+}; /* namespace rtk */
---
+++ !RTK/rtk/events/save_to_app.h
@@ -0,0 +1,102 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#ifndef _RTK_EVENTS_SAVE_TO_APP
+#define _RTK_EVENTS_SAVE_TO_APP
+
+#include <string>
+
+#include "rtk/graphics/point.h"
+#include "rtk/events/event.h"
+
+namespace rtk {
+namespace events {
+
+using rtk::graphics::point;
+
+/** An event class for requesting that data be saved to a given application. */
+class save_to_app:
+	public event
+{
+private:
+	/** The destination window handle. */
+	int _whandle;
+
+	/** The destination icon handle. */
+	int _ihandle;
+
+	/** The destination position. */
+	point _position;
+
+	/** The leafname to which the data should be saved. */
+	string _leafname;
+public:
+	/** A mixin class for handling save_to_app events.
+	 * If a class wishes to receive save_to_app events then it should
+	 * inherit from this mixin class and provide an implementation for
+	 * handle_event().
+	 */
+	class handler
+	{
+	public:
+		/** Handle save_to_app event.
+		 * @param ev the save_to_app event to be handled
+		 */
+		virtual void handle_event(save_to_app& ev)=0;
+	};
+
+	/** Construct save_to_app event.
+	 * The destination window handle, icon handle and position are obtained
+	 * using Wimp_GetPointerInfo.
+	 * @param target the target of the event
+	 * @param leafname the leafname to which the data should be saved
+	 */
+	save_to_app(desktop::component& target,const string& leafname);
+
+	/** Construct save_to_app event.
+	 * @param target the target of the event
+	 * @param whandle the destination window handle
+	 * @param ihandle the destination icon handle
+	 * @param position the destination position
+	 * @param leafname the leafname to which the data should be saved
+	 */
+	save_to_app(desktop::component& target,int whandle,int ihandle,
+		const point& position,const string& leafname);
+
+	/** Destroy save_to_app event.
+	 */
+	virtual ~save_to_app();
+
+	/** Get destination window handle.
+	 * @return the destination window handle
+	 */
+	int whandle() const
+		{ return _whandle; }
+
+	/** Get destination icon handle.
+	 * @return the destination icon handle
+	 */
+	int ihandle() const
+		{ return _ihandle; }
+
+	/** Get destination position.
+	 * @return the destination position
+	 */
+	point position() const
+		{ return _position; }
+
+	/** Get leafname.
+	 * @return the leafname to which the data should be saved
+	 */
+	const string& leafname() const
+		{ return _leafname; }
+protected:
+	virtual bool deliver(desktop::component& dest);
+};
+
+}; /* namespace events */
+}; /* namespace rtk */
+
+#endif
