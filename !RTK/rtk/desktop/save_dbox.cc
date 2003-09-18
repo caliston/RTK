95874ccfe71d6a6ca2a98a9cb2ff880a5ea25ea7
gdshaw <gdshaw@fa0f773a-f822-0410-a9df-e7ef4170f6f5>
2003-09-18 04:21:38 +0000

    Added class rtk::desktop::save_dbox.
    git-svn-id: http://source.riscpkg.org/svn/RTK/trunk@127 fa0f773a-f822-0410-a9df-e7ef4170f6f5

---
+++ !RTK/rtk/desktop/save_dbox.cc
@@ -0,0 +1,216 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#include <sstream>
+#include <iomanip>
+
+#include "rtk/os/exception.h"
+#include "rtk/os/os.h"
+#include "rtk/os/wimp.h"
+#include "rtk/desktop/save_dbox.h"
+#include "rtk/desktop/application.h"
+#include "rtk/events/save_to_file.h"
+#include "rtk/events/save_to_app.h"
+
+namespace rtk {
+namespace desktop {
+
+save_dbox::save_dbox():
+	_filetype(0xffd)
+{
+	title("Save as");
+	back_icon(false);
+	close_icon(false);
+
+	_filetype_icon.sprite_name(sprite_name(_filetype));
+	_filetype_icon.button(6);
+	_filetype_icon.xfit(false);
+	_filetype_icon.yfit(false);
+	_filetype_icon.xbaseline(xbaseline_centre);
+
+	_pathname_field.text("Untitled",255);
+	_pathname_field.validation(_pathname_field.validation()+";A~ ");
+
+	_cancel_button.text("Cancel");
+	_save_button.text("Save");
+
+	_selection_button.text("Selection");
+	_selection_button.text_and_sprite(true);
+	_selection_button.enabled(false);
+
+	_row.add(_cancel_button);
+	_row.add(_save_button);
+	_row.xgap(16);
+
+	_column.add(_filetype_icon,0);
+	_column.add(_pathname_field,1);
+	_column.add(_row,3);
+	_column.ygap(8);
+	_column.margin(16);
+
+	inherited::add(_column);
+}
+
+save_dbox::~save_dbox()
+{}
+
+void save_dbox::handle_event(events::mouse_click& ev)
+{
+	if ((ev.target()==&_filetype_icon)&&(ev.buttons()&0x50))
+	{
+		// Dragging filetype icon.
+		int flags;
+		os::OS_Byte161(28,&flags);
+		if (flags&0x02)
+		{
+			// Solid drags enabled.
+			_filetype_icon.drag_sprite(_filetype_icon.bbox(),
+				(os::sprite_area*)1,sprite_name(_filetype));
+		}
+		else
+		{
+			// Solid drags disabled.
+			_filetype_icon.drag_box(_filetype_icon.bbox());
+		}
+	}
+	else if (ev.target()==&_save_button)
+	{
+		handle_save();
+	}
+	else if (ev.target()==&_cancel_button)
+	{
+		handle_cancel();
+	}
+}
+
+void save_dbox::handle_event(events::user_drag_box& ev)
+{
+	events::save_to_app ev2(*this,leafname());
+	ev2.post();
+}
+
+void save_dbox::handle_event(events::key_pressed& ev)
+{
+	switch (ev.code())
+	{
+	case 0x0d:
+		handle_save();
+		ev.processed(true);
+		break;
+	case 0x1b:
+		handle_cancel();
+		ev.processed(true);
+		break;
+	}
+}
+
+string save_dbox::pathname() const
+{
+	return _pathname_field.text();
+}
+
+string save_dbox::leafname() const
+{
+	string pathname=_pathname_field.text();
+	string::size_type f=pathname.rfind('.');
+	if (f==string::npos) f=0; else ++f;
+	return pathname.substr(f);
+}
+
+bool save_dbox::selection_button_present() const
+{
+	return _selection_button.parent();
+}
+
+bool save_dbox::selection_button_enabled() const
+{
+	return _selection_button.enabled();
+}
+
+bool save_dbox::selection_button_state() const
+{
+	return _selection_button.selected();
+}
+
+save_dbox& save_dbox::filetype(unsigned int filetype)
+{
+	_filetype=filetype;
+	_filetype_icon.sprite_name(sprite_name(_filetype));
+	return *this;
+}
+
+save_dbox& save_dbox::pathname(const string& pathname)
+{
+	_pathname_field.text(pathname);
+	return *this;
+}
+
+save_dbox& save_dbox::selection_button_present(bool value)
+{
+	if (value)
+	{
+		_column.add(_selection_button,2);
+	}
+	else
+	{
+		_selection_button.remove();
+	}
+	return *this;
+}
+
+save_dbox& save_dbox::selection_button_enabled(bool value)
+{
+	_selection_button.enabled(value);
+	return *this;
+}
+
+save_dbox& save_dbox::selection_button_state(bool value)
+{
+	_selection_button.selected(value);
+	return *this;
+}
+
+void save_dbox::handle_save()
+{
+	string pathname=_pathname_field.text();
+	string::size_type f=pathname.find_first_of(".:<");
+	if (f!=string::npos)
+	{
+		events::save_to_file ev(*this,_pathname_field.text(),
+			_selection_button.selected());
+		ev.post();
+	}
+	else
+	{
+		application* app=parent_application();
+		const char* message="To save, drag the icon to a directory display";
+		const char* title=(app)?app->name().c_str():"Unknown";
+		os::Wimp_ReportError(0,message,title,0,0);
+	}
+}
+
+void save_dbox::handle_cancel()
+{
+	remove();
+}
+
+string save_dbox::sprite_name(unsigned int filetype)
+{
+	ostringstream out;
+	out << "file_" << hex << setw(3) << filetype;
+	string name=out.str();
+	try
+	{
+		os::Wimp_SpriteOp40(name.c_str(),0,0,0,0);
+	}
+	catch (os::exception& ex)
+	{
+		name="file_xxx";
+	}
+	return name;
+}
+
+}; /* namespace desktop */
+}; /* namespace rtk */
---
+++ !RTK/rtk/desktop/save_dbox.h
@@ -0,0 +1,162 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#ifndef _RTK_DESKTOP_SAVE_DBOX
+#define _RTK_DESKTOP_SAVE_DBOX
+
+#include "rtk/desktop/column_layout.h"
+#include "rtk/desktop/row_layout.h"
+#include "rtk/desktop/icon.h"
+#include "rtk/desktop/action_button.h"
+#include "rtk/desktop/default_button.h"
+#include "rtk/desktop/option_button.h"
+#include "rtk/desktop/writable_field.h"
+#include "rtk/desktop/window.h"
+#include "rtk/events/mouse_click.h"
+#include "rtk/events/user_drag_box.h"
+#include "rtk/events/key_pressed.h"
+
+namespace rtk {
+namespace desktop {
+
+/** A class to represent a save-as dialogue box. */
+class save_dbox:
+	public window,
+	public events::mouse_click::handler,
+	public events::user_drag_box::handler,
+	public events::key_pressed::handler
+{
+private:
+	/** The class from which this one is derived. */
+	typedef window inherited;
+
+	/** The filetype of the file to be saved. */
+	unsigned int _filetype;
+
+	/** The icon containing the filetype sprite. */
+	icon _filetype_icon;
+
+	/** The field containing the pathname/leafname. */
+	writable_field _pathname_field;
+
+	/** The cancel button. */
+	action_button _cancel_button;
+
+	/** The save button. */
+	default_button _save_button;
+
+	/** The selection button. */
+	option_button _selection_button;
+
+	/** The column containing everything. */
+	column_layout _column;
+
+	/** The row containing the cancel and save buttons. */
+	row_layout _row;
+public:
+	/** Construct save-as dialogue box.
+	 * By default, a save-as dialogue box:
+	 * - has a default filename of "Untitled"; and
+	 * - does not have a selection button.
+	 */
+	save_dbox();
+
+	/** Destroy save-as dialogue box. */
+	virtual ~save_dbox();
+
+	virtual void handle_event(events::mouse_click& ev);
+	virtual void handle_event(events::user_drag_box& ev);
+	virtual void handle_event(events::key_pressed& ev);
+
+	/** Get filetype.
+	 * @return the filetype
+	 */
+	unsigned int filetype() const
+		{ return _filetype; }
+
+	/** Get pathname.
+	 * @return the pathname
+	 */
+	string pathname() const;
+
+	/** Get leafname.
+	 * This is the final component of the pathname.
+	 * @return the leafname
+	 */
+	string leafname() const;
+
+	/** Get selection button present flag.
+	 * @return true if the dialogue box has a selection button,
+	 * otherwise false
+	 */
+	bool selection_button_present() const;
+
+	/** Get selection button enabled flag.
+	 * If false then the selection button (if present) is greyed out.
+	 * @return true if the selection button is enabled, otherwise false
+	 */
+	bool selection_button_enabled() const;
+
+	/** Get selection button state.
+	 * @return true if selection button active, otherwise false
+	 */
+	bool selection_button_state() const;
+
+	/** Set filetype.
+	 * @param filetype the required filetype
+	 */
+	save_dbox& filetype(unsigned int filetype);
+
+	/** Set pathname.
+	 * @param pathname the required pathname
+	 */
+	save_dbox& pathname(const string& pathname);
+
+	/** Set selection button present flag.
+	 * @param value true if the dialogue box is to have a selection button,
+	 *  otherwise false
+	 * @return a reference to this
+	 */
+	save_dbox& selection_button_present(bool value);
+
+	/** Set selection button enabled flag.
+	 * @param value true if the selection button is to be enabled,
+	 *  otherwise false
+	 * @return a reference to this
+	 */
+	save_dbox& selection_button_enabled(bool value);
+
+	/** Set selection button state.
+	 * @param value true if the selection button is to be active,
+	 *  otherwise false
+	 * @return a reference to this
+	 */
+	save_dbox& selection_button_state(bool value);
+private:
+	/** Handle save action.
+	 * This results from the user clicking on the save button or
+	 * pressing the return key.
+	 */
+	void handle_save();
+
+	/** Handle cancel action.
+	 * This results from the user clicking on the cancel button or
+	 * pressing the escape key.
+	 */
+	void handle_cancel();
+
+	/** Get the sprite name that corresponds to a filetype.
+	 * For a filetype of 0x123 the sprite would be 'file_123' if that
+	 * name exists in the Wimp sprite pool, otherwise 'file_xxx'.
+	 * @param filetype the filetype
+	 * @return the sprite name that corresponds to the filetype
+	 */
+	static string sprite_name(unsigned int filetype);
+};
+
+}; /* namespace desktop */
+}; /* namespace rtk */
+
+#endif
