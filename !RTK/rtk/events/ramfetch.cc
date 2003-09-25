cdd2175665e6cf79c8be983f642945bf5f7ab780
gdshaw <gdshaw@fa0f773a-f822-0410-a9df-e7ef4170f6f5>
2003-09-25 01:56:24 +0000

    Added class rtk::events::ramfetch.
    git-svn-id: http://source.riscpkg.org/svn/RTK/trunk@148 fa0f773a-f822-0410-a9df-e7ef4170f6f5

---
+++ !RTK/rtk/events/ramfetch.cc
@@ -0,0 +1,52 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#include "rtk/swi/wimp.h"
+#include "rtk/os/wimp.h"
+#include "rtk/desktop/component.h"
+#include "rtk/desktop/application.h"
+#include "rtk/events/ramfetch.h"
+
+namespace rtk {
+namespace events {
+
+using rtk::desktop::component;
+
+ramfetch::ramfetch(component& target,int wimpcode,os::wimp_block& wimpblock):
+	message(target,wimpcode,wimpblock)
+{}
+
+ramfetch::~ramfetch()
+{}
+
+void* ramfetch::buffer() const
+{
+	return reinterpret_cast<void*>(wimpblock().word[5]);
+}
+
+unsigned int ramfetch::buffer_size() const
+{
+	return wimpblock().word[6];
+}
+
+void ramfetch::reply(unsigned int count) const
+{
+	os::wimp_block block;
+	prepare_reply(block,swi::Message_RAMTransmit,npos);
+	block.word[6]=count;
+	bool last=count!=buffer_size();
+	int wimpcode=(last)?swi::User_Message:swi::User_Message_Recorded;
+	os::Wimp_SendMessage(wimpcode,block,thandle(),0,0);
+}
+
+bool ramfetch::deliver(component& dest)
+{
+	handler* h=dynamic_cast<handler*>(&dest);
+	if (h) h->handle_event(*this);
+	return h;
+}
+
+}; /* namespace events */
+}; /* namespace rtk */
---
+++ !RTK/rtk/events/ramfetch.h
@@ -0,0 +1,72 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#ifndef _RTK_EVENTS_RAMFETCH
+#define _RTK_EVENTS_RAMFETCH
+
+#include "rtk/events/message.h"
+
+namespace rtk {
+namespace os {
+
+union wimp_block;
+
+}; /* namespace os */
+
+namespace events {
+
+/** A class to represent a RISC OS Message_RAMFetch user message. */
+class ramfetch:
+	public message
+{
+public:
+	/** A mixin class for handling ramfetch events.
+	 * If a class wishes to receive ramfetch events then it should
+	 * inherit from this mixin class and provide an implementation for
+	 * handle_event().
+	 */
+	class handler
+	{
+	public:
+		/** Handle ramfetch event.
+		 * @param ev the ramfetch event to be handled
+		 */
+		virtual void handle_event(ramfetch& ev)=0;
+	};
+
+	/** Construct ramfetch event.
+	 * @param target the target of the event
+	 * @param wimpcode the Wimp event code
+	 * @param wimpblock the Wimp event block
+	 */
+	ramfetch(desktop::component& target,int wimpcode,
+		os::wimp_block& wimpblock);
+
+	/** Destroy ramfetch event.
+	 */
+	virtual ~ramfetch();
+protected:
+	virtual bool deliver(desktop::component& dest);
+public:
+	/** Get data transfer buffer.
+	 * @return the data transfer buffer
+	 */
+	void* buffer() const;
+
+	/** Get size of the data transfer buffer.
+	 * @return the size of the data transfer buffer
+	 */
+	unsigned int buffer_size() const;
+
+	/** Send Message_RAMTransmit in reply to this event.
+	 * @param count the number of bytes transferred
+	 */
+	void reply(unsigned int count) const;
+};
+
+}; /* namespace events */
+}; /* namespace rtk */
+
+#endif
