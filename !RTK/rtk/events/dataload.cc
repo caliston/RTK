4503661f2160c2d957f2ef4028b830b117717b76
gdshaw <gdshaw@fa0f773a-f822-0410-a9df-e7ef4170f6f5>
2003-09-25 01:53:00 +0000

    Added class rtk::events::dataload.
    git-svn-id: http://source.riscpkg.org/svn/RTK/trunk@146 fa0f773a-f822-0410-a9df-e7ef4170f6f5

---
+++ !RTK/rtk/events/dataload.cc
@@ -0,0 +1,69 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#include "rtk/swi/wimp.h"
+#include "rtk/os/wimp.h"
+#include "rtk/desktop/component.h"
+#include "rtk/desktop/application.h"
+#include "rtk/events/dataload.h"
+
+namespace rtk {
+namespace events {
+
+using rtk::desktop::component;
+
+dataload::dataload(component& target,int wimpcode,os::wimp_block& wimpblock):
+	message(target,wimpcode,wimpblock)
+{}
+
+dataload::~dataload()
+{}
+
+int dataload::whandle() const
+{
+	return wimpblock().word[5];
+}
+
+int dataload::ihandle() const
+{
+	return wimpblock().word[6];
+}
+
+point dataload::position() const
+{
+	return point(wimpblock().word[7],wimpblock().word[8]);
+}
+
+unsigned int dataload::estsize() const
+{
+	return wimpblock().word[9];
+}
+
+unsigned int dataload::filetype() const
+{
+	return wimpblock().word[10];
+}
+
+string dataload::pathname() const
+{
+	return string(wimpblock().byte+44);
+}
+
+void dataload::reply() const
+{
+	static os::wimp_block block;
+	prepare_reply(block,swi::Message_DataLoadAck,npos);
+	os::Wimp_SendMessage(swi::User_Message,block,thandle(),0,0);
+}
+
+bool dataload::deliver(component& dest)
+{
+	handler* h=dynamic_cast<handler*>(&dest);
+	if (h) h->handle_event(*this);
+	return h;
+}
+
+}; /* namespace events */
+}; /* namespace rtk */
---
+++ !RTK/rtk/events/dataload.h
@@ -0,0 +1,94 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#ifndef _RTK_EVENTS_DATALOAD
+#define _RTK_EVENTS_DATALOAD
+
+#include <string>
+
+#include "rtk/graphics/point.h"
+#include "rtk/events/message.h"
+
+namespace rtk {
+namespace os {
+
+union wimp_block;
+
+}; /* namespace os */
+
+namespace events {
+
+using rtk::graphics::point;
+
+/** A class to represent a RISC OS Message_DataLoad user message. */
+class dataload:
+	public message
+{
+public:
+	/** A mixin class for handling dataload events.
+	 * If a class wishes to receive dataload events then it should
+	 * inherit from this mixin class and provide an implementation for
+	 * handle_event().
+	 */
+	class handler
+	{
+	public:
+		/** Handle dataload event.
+		 * @param ev the dataload event to be handled
+		 */
+		virtual void handle_event(dataload& ev)=0;
+	};
+
+	/** Construct dataload event.
+	 * @param target the target of the event
+	 * @param wimpcode the Wimp event code
+	 * @param wimpblock the Wimp event block
+	 */
+	dataload(desktop::component& target,int wimpcode,
+		os::wimp_block& wimpblock);
+
+	/** Destroy dataload event. */
+	virtual ~dataload();
+protected:
+	virtual bool deliver(desktop::component& dest);
+public:
+	/** Get destination window handle.
+	 * @return the destination window handle
+	 */
+	int whandle() const;
+
+	/** Get destination icon handle.
+	 * @return the destination icon handle
+	 */
+	int ihandle() const;
+
+	/** Get destination position.
+	 * @return the destination position
+	 */
+	point position() const;
+
+	/** Get estimated size of data.
+	 * @return the estimated size of the data
+	 */
+	unsigned int estsize() const;
+
+	/** Get filetype of data.
+	 * @return the filetype of the data
+	 */
+	unsigned int filetype() const;
+
+	/** Get pathname for data.
+	 * @return the pathname
+	 */
+	string pathname() const;
+
+	/** Send Message_DataLoadAck in reply to this event. */
+	void reply() const;
+};
+
+}; /* namespace events */
+}; /* namespace rtk */
+
+#endif
