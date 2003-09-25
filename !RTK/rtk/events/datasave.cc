8029a51d1455c16672b8b688645bb0dbf91f999d
gdshaw <gdshaw@fa0f773a-f822-0410-a9df-e7ef4170f6f5>
2003-09-25 01:48:34 +0000

    Added class rtk::events::datasave.
    git-svn-id: http://source.riscpkg.org/svn/RTK/trunk@144 fa0f773a-f822-0410-a9df-e7ef4170f6f5

---
+++ !RTK/rtk/events/datasave.cc
@@ -0,0 +1,81 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#include "rtk/swi/wimp.h"
+#include "rtk/os/wimp.h"
+#include "rtk/desktop/component.h"
+#include "rtk/desktop/application.h"
+#include "rtk/events/datasave.h"
+
+namespace rtk {
+namespace events {
+
+using rtk::desktop::component;
+
+datasave::datasave(component& target,int wimpcode,os::wimp_block& wimpblock):
+	message(target,wimpcode,wimpblock)
+{}
+
+datasave::~datasave()
+{}
+
+int datasave::whandle() const
+{
+	return wimpblock().word[5];
+}
+
+int datasave::ihandle() const
+{
+	return wimpblock().word[6];
+}
+
+point datasave::position() const
+{
+	return point(wimpblock().word[7],wimpblock().word[8]);
+}
+
+unsigned int datasave::estsize() const
+{
+	return wimpblock().word[9];
+}
+
+unsigned int datasave::filetype() const
+{
+	return wimpblock().word[10];
+}
+
+string datasave::leafname() const
+{
+	return string(wimpblock().byte+44);
+}
+
+void datasave::reply(void* buffer,unsigned int buffer_size) const
+{
+	os::wimp_block block;
+	prepare_reply(block,swi::Message_RAMFetch,28);
+	block.word[5]=reinterpret_cast<int>(buffer);
+	block.word[6]=buffer_size;
+	os::Wimp_SendMessage(swi::User_Message_Recorded,block,thandle(),0,0);
+}
+
+void datasave::reply(const string& pathname) const
+{
+	os::wimp_block block;
+	prepare_reply(block,swi::Message_DataSaveAck,npos);
+	unsigned int i=pathname.copy(block.byte+44,211);
+	block.byte[44+i]=0;
+	block.word[0]=44+((i+4)&~3);
+	os::Wimp_SendMessage(swi::User_Message,block,thandle(),0,0);
+}
+
+bool datasave::deliver(component& dest)
+{
+	handler* h=dynamic_cast<handler*>(&dest);
+	if (h) h->handle_event(*this);
+	return h;
+}
+
+}; /* namespace events */
+}; /* namespace rtk */
---
+++ !RTK/rtk/events/datasave.h
@@ -0,0 +1,103 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#ifndef _RTK_EVENTS_DATASAVE
+#define _RTK_EVENTS_DATASAVE
+
+#include <string>
+
+#include "rtk/graphics/point.h"
+#include "rtk/events/message.h"
+
+namespace rtk {
+namespace os {
+
+union wimp_block;
+
+}; /* namespace os */
+
+namespace events {
+
+using rtk::graphics::point;
+
+/** A class to represent a RISC OS Message_DataSave user message. */
+class datasave:
+	public message
+{
+public:
+	/** A mixin class for handling datasave events.
+	 * If a class wishes to receive datasave events then it should
+	 * inherit from this mixin class and provide an implementation for
+	 * handle_event().
+	 */
+	class handler
+	{
+	public:
+		/** Handle datasave event.
+		 * @param ev the datasave event to be handled
+		 */
+		virtual void handle_event(datasave& ev)=0;
+	};
+
+	/** Construct datasave event.
+	 * @param target the target of the event
+	 * @param wimpcode the Wimp event code
+	 * @param wimpblock the Wimp event block
+	 */
+	datasave(desktop::component& target,int wimpcode,
+		os::wimp_block& wimpblock);
+
+	/** Destroy datasave event.
+	 */
+	virtual ~datasave();
+protected:
+	virtual bool deliver(desktop::component& dest);
+public:
+	/** Get destination window handle.
+	 * @return the destination window handle
+	 */
+	int whandle() const;
+
+	/** Get destination icon handle.
+	 * @return the destination icon handle
+	 */
+	int ihandle() const;
+
+	/** Get destination position.
+	 * @return the destination position
+	 */
+	point position() const;
+
+	/** Get estimated size of data.
+	 * @return the estimated size of the data
+	 */
+	unsigned int estsize() const;
+
+	/** Get filetype of data.
+	 * @return the filetype of the data
+	 */
+	unsigned int filetype() const;
+
+	/** Get proposed leafname for data.
+	 * @return the proposed leafname
+	 */
+	string leafname() const;
+
+	/** Send Message_RAMFetch in reply to this event.
+	 * @param buffer the data transfer buffer
+	 * @param buffer_size the size of the data transfer buffer
+	 */
+	void reply(void* buffer,unsigned int buffer_size) const;
+
+	/** Send Message_DataSaveAck in reply to this event.
+	 * @param pathname the required pathname
+	 */
+	void reply(const string& pathname) const;
+};
+
+}; /* namespace events */
+}; /* namespace rtk */
+
+#endif
