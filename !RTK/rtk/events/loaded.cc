302c3d909dd034655a273256043c78b6f1329915
gdshaw <gdshaw@fa0f773a-f822-0410-a9df-e7ef4170f6f5>
2003-09-28 06:50:31 +0000

    Added class rtk::events::loaded.
    git-svn-id: http://source.riscpkg.org/svn/RTK/trunk@151 fa0f773a-f822-0410-a9df-e7ef4170f6f5

---
+++ !RTK/rtk/events/loaded.cc
@@ -0,0 +1,31 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#include "rtk/os/wimp.h"
+#include "rtk/desktop/component.h"
+#include "rtk/events/loaded.h"
+
+namespace rtk {
+namespace events {
+
+using rtk::desktop::component;
+
+loaded::loaded(component& target,transfer::load& loadop):
+	event(target),
+	_loadop(&loadop)
+{};
+
+loaded::~loaded()
+{}
+
+bool loaded::deliver(component& dest)
+{
+	handler* h=dynamic_cast<handler*>(&dest);
+	if (h) h->handle_event(*this);
+	return h;
+}
+
+}; /* namespace events */
+}; /* namespace rtk */
---
+++ !RTK/rtk/events/loaded.h
@@ -0,0 +1,65 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#ifndef _RTK_EVENTS_LOADED
+#define _RTK_EVENTS_LOADED
+
+#include <string>
+
+#include "rtk/events/event.h"
+
+namespace rtk {
+namespace transfer {
+
+class load;
+
+}; /* namespace transfer */
+
+namespace events {
+
+/** An event class for reporting that a file has been loaded. */
+class loaded:
+	public event
+{
+private:
+	/** The load operation that loaded the file. */
+	transfer::load* _loadop;
+public:
+	/** A mixin class for handling loaded events.
+	 * If a class wishes to receive loaded events then it should
+	 * inherit from this mixin class and provide an implementation for
+	 * handle_event().
+	 */
+	class handler
+	{
+	public:
+		/** Handle loaded event.
+		 * @param ev the loaded event to be handled
+		 */
+		virtual void handle_event(loaded& ev)=0;
+	};
+
+	/** Construct loaded event.
+	 * @param target the target of the event
+	 * @param loadop the load operation that loaded the file
+	 */
+	loaded(desktop::component& target,transfer::load& loadop);
+
+	/** Destroy loaded event. */
+	virtual ~loaded();
+
+	/** Get load operation.
+	 * @return the load operation that loaded the file
+	 */
+	transfer::load& loadop()
+		{ return *_loadop; }
+protected:
+	virtual bool deliver(desktop::component& dest);
+};
+
+}; /* namespace events */
+}; /* namespace rtk */
+
+#endif
