bdfb9eb8692d64daba196e3bfd9bad96c315f1ba
gdshaw <gdshaw@fa0f773a-f822-0410-a9df-e7ef4170f6f5>
2003-09-28 23:19:59 +0000

    Added class rtk::transfer::save.
    git-svn-id: http://source.riscpkg.org/svn/RTK/trunk@155 fa0f773a-f822-0410-a9df-e7ef4170f6f5

---
+++ !RTK/rtk/transfer/save.cc
@@ -0,0 +1,238 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#include <string.h>
+#include <algorithm>
+
+#include "rtk/swi/wimp.h"
+#include "rtk/os/os.h"
+#include "rtk/os/wimp.h"
+#include "rtk/desktop/application.h"
+#include "rtk/events/wimp.h"
+#include "rtk/events/message.h"
+#include "rtk/events/datasave.h"
+#include "rtk/events/dataload.h"
+#include "rtk/events/ramtransmit.h"
+#include "rtk/events/save_to_app.h"
+#include "rtk/events/save_to_file.h"
+#include "rtk/events/saved.h"
+#include "rtk/transfer/save.h"
+
+namespace rtk {
+namespace transfer {
+
+save::save():
+	_state(state_idle),
+	_thandle(0),
+	_filetype(0xffd),
+	_allow_ram_transfer(true),
+	_secure(false),
+	_ldata(0),
+	_lsize(0)
+{
+	os::Wimp_ReadSysInfo(5,&_thandle,0);
+}
+
+save::~save()
+{}
+
+graphics::box save::min_bbox() const
+{
+	return graphics::box();
+}
+
+void save::handle_event(events::datasaveack& ev)
+{
+	// A Message_DataSaveAck is acted upon in response to a Message_DataSave.
+	if (_state==state_datasave)
+	{
+		get_file(ev.pathname());
+		_pathname=ev.pathname();
+		_secure=ev.estsize()!=static_cast<size_type>(-1);
+		ev.reply();
+		_state=state_dataload;
+	}
+}
+
+void save::handle_event(events::dataloadack& ev)
+{
+	// A Message_DataLoadAck is acted upon in response to a Message_DataLoad.
+	if (_state==state_dataload)
+	{
+		_state=state_idle;
+		events::saved ev2(*this,*this);
+		ev2.post();
+	}
+}
+
+void save::handle_event(events::ramfetch& ev)
+{
+	// A Message_RAMFetch is acted upon in response to a Message_DataSave
+	// or a Message_RAMTransmit, but only if RAM transfers are enabled.
+	if (((_state==state_datasave)||(_state==state_ramtransmit))&&
+		_allow_ram_transfer)
+	{
+		size_type rfree=ev.buffer_size();
+		void* rdata=ev.buffer();
+		if (!_lsize) get_block(&_ldata,&_lsize);
+		while (rfree&&_lsize)
+		{
+			size_type count=min(rfree,_lsize);
+			os::Wimp_TransferBlock(_thandle,_ldata,ev.thandle(),rdata,count);
+			rdata=static_cast<char*>(rdata)+count;
+			_ldata=static_cast<const char*>(_ldata)+count;
+			rfree-=count;
+			_lsize-=count;
+			if (!_lsize) get_block(&_ldata,&_lsize);
+		}
+		ev.reply(ev.buffer_size()-rfree);
+		if (rfree)
+		{
+			// If there is space at the end of the buffer then this is
+			// the last message and no reply is expected.
+			finish();
+			_state=state_idle;
+			events::saved ev2(*this,*this);
+			ev2.post();
+		}
+		else
+		{
+			// If there is no space at the end of the buffer then at
+			// least one more Message_RAMFetch is expected.
+			_state=state_ramtransmit;
+		}
+	}
+}
+
+void save::handle_event(events::save_to_app& ev)
+{
+	os::wimp_block& block=*new os::wimp_block;
+	block.word[3]=0;
+	block.word[4]=swi::Message_DataSave;
+	block.word[5]=ev.whandle();
+	block.word[6]=ev.ihandle();
+	block.word[7]=ev.position().x();
+	block.word[8]=ev.position().y();
+	block.word[9]=estsize();
+	block.word[10]=filetype();
+	unsigned int i=ev.leafname().copy(block.byte+44,211);
+	block.byte[44+i]=0;
+	block.word[0]=44+((i+4)&~3);
+	if (desktop::application* app=ev.target()->parent_application())
+		app->send_message(swi::User_Message,block,ev.whandle(),ev.ihandle());
+	start();
+	_ldata=0;
+	_lsize=0;
+	_pathname=string();
+	_secure=false;
+	_state=state_datasave;
+}
+
+void save::handle_event(events::save_to_file& ev)
+{
+	_pathname=string();
+	_secure=false;
+	get_file(ev.pathname());
+	_pathname=ev.pathname();
+	_secure=true;
+	_state=state_idle;
+}
+
+save& save::filetype(unsigned int filetype)
+{
+	_filetype=filetype;
+	return *this;
+}
+
+save& save::allow_ram_transfer(bool value)
+{
+	_allow_ram_transfer=value;
+	return *this;
+}
+
+void save::deliver_wimp_block(int wimpcode,os::wimp_block& wimpblock)
+{
+	switch (wimpcode)
+	{
+	case 17:
+	case 18:
+	case 19:
+		deliver_message(wimpcode,wimpblock);
+		break;
+	default:
+		{ 
+			events::wimp ev(*this,wimpcode,wimpblock);
+			ev.post();
+		}
+		break;
+	}
+}
+
+void save::deliver_message(int wimpcode,os::wimp_block& wimpblock)
+{
+	switch (wimpblock.word[4])
+	{
+	case swi::Message_DataSave:
+		{
+			events::datasave ev(*this,wimpcode,wimpblock);
+			ev.post();
+		}
+		break;
+	case swi::Message_DataSaveAck:
+		{
+			events::datasaveack ev(*this,wimpcode,wimpblock);
+			ev.post();
+		}
+		break;
+	case swi::Message_DataLoad:
+		{
+			events::dataload ev(*this,wimpcode,wimpblock);
+			ev.post();
+		}
+		break;
+	case swi::Message_DataLoadAck:
+		{
+			events::dataloadack ev(*this,wimpcode,wimpblock);
+			ev.post();
+		}
+		break;
+	case swi::Message_RAMFetch:
+		{
+			events::ramfetch ev(*this,wimpcode,wimpblock);
+			ev.post();
+		}
+		break;
+	case swi::Message_RAMTransmit:
+		{
+			events::ramtransmit ev(*this,wimpcode,wimpblock);
+			ev.post();
+		}
+		break;
+	default:
+		{
+			events::message ev(*this,wimpcode,wimpblock);
+			ev.post();
+		}
+		break;
+	}
+}
+
+void save::get_file(const string& pathname)
+{
+	start();
+	int fhandle;
+	os::OS_Find(0x83,pathname.c_str(),0,&fhandle);
+	get_block(&_ldata,&_lsize);
+	while (_lsize)
+	{
+		os::OS_GBPB2(fhandle,_ldata,_lsize,0);
+		get_block(&_ldata,&_lsize);
+	}
+	os::OS_Find0(fhandle);
+	finish();
+}
+
+}; /* namespace transfer */
+}; /* namespace rtk */
---
+++ !RTK/rtk/transfer/save.h
@@ -0,0 +1,199 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#ifndef _RTK_TRANSFER_SAVE
+#define _RTK_TRANSFER_SAVE
+
+#include "rtk/desktop/component.h"
+#include "rtk/events/datasaveack.h"
+#include "rtk/events/dataloadack.h"
+#include "rtk/events/ramfetch.h"
+#include "rtk/events/save_to_app.h"
+#include "rtk/events/save_to_file.h"
+#include "rtk/events/redirection.h"
+
+namespace rtk {
+namespace os {
+
+union wimp_block;
+
+}; /* namespace os */
+
+namespace transfer {
+
+/** An abstract class to represent a save operation using the
+ * data transfer protocol.
+ * Implementations must override start(), get(), finish() and estsize()
+ * to define the source of the data.
+ */
+class save:
+	public desktop::component,
+	public events::datasaveack::handler,
+	public events::dataloadack::handler,
+	public events::ramfetch::handler,
+	public events::save_to_app::handler,
+	public events::save_to_file::handler,
+	public events::redirection
+{
+public:
+	/** A type for representing byte counts. */
+	typedef unsigned int size_type;
+
+	/** A null value for use in place of a byte count. */
+	static const size_type npos=static_cast<size_type>(-1);
+private:
+	/** An enumeration to represent the current state of the
+	 * save operation. */
+	enum state_type
+	{
+		/** The state in which the save operation is waiting for an RTK
+		 * save_to_file or save_to_app event. */
+		state_idle,
+		/** The state in which the save operation has sent a Message_DataSave
+		 * and is waiting for either Message_RAMFetch or Message_DataSaveAck.
+		 */
+		state_datasave,
+		/** The state in which the save operation has sent a Message_DataLoad
+		 * and is waiting for a Message_DataLoadAck. */
+		state_dataload,
+		/** The state in which the save operation has sent an initial or
+		 * intermediate Message_RAMTransmit and is waiting for a
+		 * Message_RAMFetch. */
+		state_ramtransmit
+	};
+
+	/** The current state of the save operation. */
+	state_type _state;
+
+	/** The local task handle. */
+	int _thandle;
+
+	/** The filetype of the data to be saved. */
+	unsigned int _filetype;
+
+	/** The RAM transfer allowed flag. */
+	bool _allow_ram_transfer;
+
+	/** The pathname to which the data was most recently saved. */
+	string _pathname;
+
+	/** The data secure flag. */
+	bool _secure;
+
+	/** The current local data buffer. */
+	const void* _ldata;
+
+	/** The current local data buffer size. */
+	size_type _lsize;
+public:
+	/** Construct save object. */
+	save();
+
+	/** Destroy save object. */
+	virtual ~save();
+
+	virtual graphics::box min_bbox() const;
+
+	virtual void handle_event(events::datasaveack& ev);
+	virtual void handle_event(events::dataloadack& ev);
+	virtual void handle_event(events::ramfetch& ev);
+	virtual void handle_event(events::save_to_app& ev);
+	virtual void handle_event(events::save_to_file& ev);
+
+	/** Get filetype.
+	 * @return the filetype of the data to be saved
+	 */
+	unsigned int filetype() const
+		{ return _filetype; }
+
+	/** Get RAM transfer allowed flag.
+	 * @return true if RAM transfers are allowed, otherwise false.
+	 */
+	bool allow_ram_transfer() const
+		{ return _allow_ram_transfer; }
+
+	/** Get pathname.
+	 * This is set to the empty string if the data has not been saved,
+	 * or if the most recent save operation used the RAM transfer
+	 * protocol.
+	 * @return the pathname to which the data was most recently saved
+	 */
+	const string& pathname() const
+		{ return _pathname; }
+
+	/** Get data secure flag.
+	 * The date is secure if it has been written to a parmanent file
+	 * (as opposed to a temporary file such as <Wimp$Scrap>).
+	 * @return the data secure flag
+	 */
+	bool secure() const
+		{ return _secure; }
+
+	/** Set filetype.
+	 * @param filetype the filetype of the data to be saved.
+	 */
+	save& filetype(unsigned int filetype);
+
+	/** Set RAM transfer allowed flag.
+	 * Applications are not obliged to support RAM transfers, but nor is
+	 * any extra work needed to implement them when using this class.
+	 * They should be enabled unless there is a good reason not to. 
+	 * @param value true if RAM transfers are allowed, otherwise false.
+	 * @return a reference to this
+	 */
+	save& allow_ram_transfer(bool value);
+
+	/** Deliver Wimp event block.
+	 * @internal
+	 * @param wimpcode the Wimp event code
+	 * @param wimpblock the Wimp event block
+	 */
+	void deliver_wimp_block(int wimpcode,os::wimp_block& wimpblock);
+
+	/** Deliver Wimp message.
+	 * @internal
+	 * @param wimpcode the Wimp event code
+	 * @param wimpblock the Wimp event block
+	 */
+	void deliver_message(int wimpcode,os::wimp_block& wimpblock);
+protected:
+	/** Start new save operation.
+	 * If start() is called before a previous operation has been completed
+	 * then the previous operation should be aborted.
+	 */
+	virtual void start()=0;
+
+	/** Get data as memory block.
+	 * A memory block is returned containing some or all of the remaining
+	 * data.  To obtain all of the data, repeat until the returned count
+	 * is zero.  
+	 * @param data the buffer for the returned block pointer
+	 * @param count a buffer for the returned block length in bytes
+	 */
+	virtual void get_block(const void** data,size_type* count)=0;
+
+	/** Finish save operation. */
+	virtual void finish()=0;
+
+	/** Get estimated size.
+	 * @return the estimated size of the data
+	 */
+	virtual size_type estsize()=0;
+
+	/** Get data as file.
+	 * All data is copied to the specified pathname.
+	 * The default implementation of this method uses read() to
+	 * fetch the data and OS_GBPB to write it to disc.
+	 * It can be overridden if a more efficient method is known,
+	 * but there is no obligation to do so.
+	 * @param pathname the pathname to which the data should be copied
+	 */
+	virtual void get_file(const string& pathname);
+};
+
+}; /* namespace transfer */
+}; /* namespace rtk */
+
+#endif
