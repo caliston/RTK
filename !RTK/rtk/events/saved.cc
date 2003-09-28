8347edad7c9ac7be49c0bfe8acca347c3c0756c7
gdshaw <gdshaw@fa0f773a-f822-0410-a9df-e7ef4170f6f5>
2003-09-28 06:52:49 +0000

    Added class rtk::events::saved.
    git-svn-id: http://source.riscpkg.org/svn/RTK/trunk@152 fa0f773a-f822-0410-a9df-e7ef4170f6f5

---
+++ !RTK/rtk/events/saved.cc
@@ -0,0 +1,31 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#include "rtk/os/wimp.h"
+#include "rtk/desktop/component.h"
+#include "rtk/events/saved.h"
+
+namespace rtk {
+namespace events {
+
+using rtk::desktop::component;
+
+saved::saved(component& target,transfer::save& saveop):
+	event(target),
+	_saveop(&saveop)
+{};
+
+saved::~saved()
+{}
+
+bool saved::deliver(component& dest)
+{
+	handler* h=dynamic_cast<handler*>(&dest);
+	if (h) h->handle_event(*this);
+	return h;
+}
+
+}; /* namespace events */
+}; /* namespace rtk */
---
+++ !RTK/rtk/events/saved.h
@@ -0,0 +1,65 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2003 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#ifndef _RTK_EVENTS_SAVED
+#define _RTK_EVENTS_SAVED
+
+#include <string>
+
+#include "rtk/events/event.h"
+
+namespace rtk {
+namespace transfer {
+
+class save;
+
+}; /* namespace transfer */
+
+namespace events {
+
+/** An event class for reporting that a file has been saved. */
+class saved:
+	public event
+{
+private:
+	/** The save operation that saved the file. */
+	transfer::save* _saveop;
+public:
+	/** A mixin class for handling saved events.
+	 * If a class wishes to receive saved events then it should
+	 * inherit from this mixin class and provide an implementation for
+	 * handle_event().
+	 */
+	class handler
+	{
+	public:
+		/** Handle saved event.
+		 * @param ev the saved event to be handled
+		 */
+		virtual void handle_event(saved& ev)=0;
+	};
+
+	/** Construct saved event.
+	 * @param target the target of the event
+	 * @param saveop the saved operation that saved the file
+	 */
+	saved(desktop::component& target,transfer::save& saveop);
+
+	/** Destroy saved event. */
+	virtual ~saved();
+
+	/** Get save operation.
+	 * @return the save operation that saved the file
+	 */
+	transfer::save& saveop()
+		{ return *_saveop; }
+protected:
+	virtual bool deliver(desktop::component& dest);
+};
+
+}; /* namespace events */
+}; /* namespace rtk */
+
+#endif
