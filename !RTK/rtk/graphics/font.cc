70ffdbb3a72d2eaa457f408c64ceacb070dc4a66
gdshaw <gdshaw@fa0f773a-f822-0410-a9df-e7ef4170f6f5>
2004-12-02 17:17:24 +0000

    Added support for desktop and symbol fonts.
    git-svn-id: http://source.riscpkg.org/svn/RTK/trunk@312 fa0f773a-f822-0410-a9df-e7ef4170f6f5

--- !RTK/rtk/graphics/font.cc
+++ !RTK/rtk/graphics/font.cc
@@ -1,59 +1,145 @@
 // This file is part of the RISC OS Toolkit (RTK).
-// Copyright © 2003 Graham Shaw.
+// Copyright © 2003-2004 Graham Shaw.
 // Distribution and use are subject to the GNU Lesser General Public License,
 // a copy of which may be found in the file !RTK.Copyright.
 
 #include "rtk/graphics/point.h"
 #include "rtk/graphics/font.h"
+
 #include "rtk/os/font.h"
 #include "rtk/os/colourtrans.h"
+#include "rtk/os/wimp.h"
 
 namespace rtk {
 namespace graphics {
 
-struct font::_font
+class font::basic_font
 {
-	int refcount;
-	int handle;
+	friend font;
+private:
+	unsigned int _refcount;
+public:
+	basic_font(unsigned int refcount);
+	virtual ~basic_font();
+	virtual int handle() const=0;
+	static basic_font* select_default_font(default_font_type type);
 };
 
-font::font(const string& id,int xsize,int ysize):
-	_f(0)
+class font::desktop_font:
+	public font::basic_font
 {
-	try
+public:
+	desktop_font();
+	virtual int handle() const;
+};
+
+class font::symbol_font:
+	public font::basic_font
+{
+public:
+	symbol_font();
+	virtual int handle() const;
+};
+
+class font::custom_font:
+	public font::basic_font
+{
+private:
+	int _handle;
+public:
+	custom_font(const string& id,int xsize,int ysize);
+	virtual ~custom_font();
+	virtual int handle() const;
+};
+
+font::basic_font::basic_font(unsigned int refcount):
+	_refcount(refcount)
+{}
+
+font::basic_font::~basic_font()
+{}
+
+font::basic_font* font::basic_font::select_default_font(default_font_type type)
+{
+	static desktop_font df;
+	static symbol_font sf;
+	switch (type)
 	{
-		_f=new _font;
-		os::Font_FindFont(id.c_str(),xsize,ysize,0,0,&_f->handle,0,0);
+	case font_desktop:
+		return &df;
+	case font_symbol:
+		return &sf;
+	default:
+		return 0;
 	}
-	catch (...)
-	{
-		delete _f;
-	}
+}
+
+font::desktop_font::desktop_font():
+	basic_font(1)
+{}
+
+int font::desktop_font::handle() const
+{
+	int handle=0;
+	os::Wimp_ReadSysInfo(8,&handle,0);
+	return handle;
+}
+
+font::symbol_font::symbol_font():
+	basic_font(1)
+{}
+
+int font::symbol_font::handle() const
+{
+	int handle=0;
+	os::Wimp_ReadSysInfo(8,0,&handle);
+	return handle;
+}
+
+font::custom_font::custom_font(const string& id,int xsize,int ysize):
+	basic_font(0),
+	_handle(0)
+{
+	os::Font_FindFont(id.c_str(),xsize,ysize,0,0,&_handle,0,0);
+}
+
+font::custom_font::~custom_font()
+{
+	os::Font_LoseFont(_handle);
+}
+
+int font::custom_font::handle() const
+{
+	return _handle;
+}
+
+font::font(const string& id,int xsize,int ysize):
+	_f(new custom_font(id,xsize,ysize))
+{
+	++_f->_refcount;
+}
+
+font::font(default_font_type type):
+	_f(basic_font::select_default_font(type))
+{
+	++_f->_refcount;
 }
 
 font::font(const font& f):
 	_f(f._f)
 {
-	++_f->refcount;
+	++_f->_refcount;
 }
 
 font::~font()
 {
-	if (!--_f->refcount)
-	{
-		os::Font_LoseFont(_f->handle);
-		delete _f;
-	}
+	if (!--_f->_refcount) delete _f;
 }
 
 font& font::operator=(const font& f)
 {
-	++f._f->refcount;
-	if (!--_f->refcount)
-	{
-		os::Font_LoseFont(_f->handle);
-		delete _f;
-	}
+	++f._f->_refcount;
+	if (!--_f->_refcount) delete _f;
 	_f=f._f;
 	return *this;
 }
@@ -63,20 +149,22 @@ void font::paint(const string& s,const point& p) const
 	// Note: s.c_str() is used in preference to s.data() because
 	// Font_Paint must be able to access the character following
 	// the end of the string.
-	os::ColourTrans_SetFontColours(_f->handle,0xffffff00,0x00000000,14,0,0,0);
-	os::Font_Paint(_f->handle,s.c_str(),0x190,p,0,0,s.length());
+	int handle=_f->handle();
+	os::ColourTrans_SetFontColours(handle,0xffffff00,0x00000000,14,0,0,0);
+	os::Font_Paint(handle,s.c_str(),0x190,p,0,0,s.length());
 }
 
-int font::width(const string& s) const
+int font::width(const char* s,unsigned int length) const
 {
-	// Note: s.c_str() is used in preference to s.data() because
-	// Font_Paint must be able to access the character following
-	// the end of the string.
-	const int maxint=0xffffff;
+	int handle=_f->handle();
 	point size;
-	os::Font_ScanString(_f->handle,s.c_str(),0x180,point(maxint,maxint),0,0,
-		s.length(),0,&size,0);
+	os::Font_ScanString(handle,s,0x180,point(-1,0),0,0,length,0,&size,0);
 	return size.x()/400;
+}
+
+int font::handle() const
+{
+	return _f->handle();
 }
 
 }; /* namespace graphics */
--- !RTK/rtk/graphics/font.h
+++ !RTK/rtk/graphics/font.h
@@ -1,5 +1,5 @@
 // This file is part of the RISC OS Toolkit (RTK).
-// Copyright © 2003 Graham Shaw.
+// Copyright © 2003-2004 Graham Shaw.
 // Distribution and use are subject to the GNU Lesser General Public License,
 // a copy of which may be found in the file !RTK.Copyright.
 
@@ -15,11 +15,31 @@ class point;
 
 class font
 {
+public:
+	/** An enumation for specifying one of the default fonts. */
+	enum default_font_type
+	{
+		/** An enumeration constant to represent the current desktop font. */
+		font_desktop,
+		/** An enumeration constant to represent the current symbol font. */
+		font_symbol
+	};
 private:
-	/** A reference-counted structure for sharing font data. */
-	struct _font;
+	/** A reference-counted class for sharing font data. */
+	struct basic_font;
+
+	/** A subclass of basic_font to represent the current desktop font. */
+	struct desktop_font;
+
+	/** A subclass of basic_font to represent the current symbol font. */
+	struct symbol_font;
+
+	/** A subclass of basic_font to represent a font specified by ID
+	 * and size. */
+	struct custom_font;
+
 	/** A pointer to the shared font data. */
-	_font* _f;
+	basic_font* _f;
 public:
 	/** Construct font object.
 	 * @param id the font identifier
@@ -27,6 +47,11 @@ public:
 	 * @param ysize the required font height (16 units = 1 point)
 	 */
 	font(const string& id,int xsize,int ysize);
+
+	/** Construct font object, referring to a default font
+	 * @param default_font a default font type (desktop or symbol)
+	 */
+	font(default_font_type default_font);
 
 	/** Construct copy of font object.
 	 * Font objects may be freely copied.  The underlying font handle
@@ -54,9 +79,22 @@ public:
 
 	/** Get width required to plot string using font.
 	 * @param s the string to plot
+	 * @param length the length of the string to plot
 	 * @return the required width (in OS units)
 	 */
-	int width(const string& s) const;
+	int width(const char* s,unsigned int length) const;
+
+	/** Get width required to plot string using font.
+	 * @param s the string to plot
+	 * @return the required width (in OS units)
+	 */
+	int width(const string& s) const
+		{ return width(s.c_str(),s.length()); }
+
+	/** Get font handle.
+	 * @return the handle for this font
+	 */
+	int handle() const;
 };
 
 }; /* namespace graphics */
