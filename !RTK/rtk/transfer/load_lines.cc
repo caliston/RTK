b3ff9307e6ebd34999486084d2ba32ebbbc22a9f
gdshaw <gdshaw@fa0f773a-f822-0410-a9df-e7ef4170f6f5>
2004-12-02 21:48:15 +0000

    Added class transfer::load_lines.
    git-svn-id: http://source.riscpkg.org/svn/RTK/trunk@317 fa0f773a-f822-0410-a9df-e7ef4170f6f5

---
+++ !RTK/rtk/transfer/load_lines.cc
@@ -0,0 +1,104 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2004 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#include "rtk/swi/wimp.h"
+#include "rtk/os/os.h"
+#include "rtk/os/wimp.h"
+#include "rtk/desktop/application.h"
+#include "rtk/events/wimp.h"
+#include "rtk/events/message.h"
+#include "rtk/events/ramfetch.h"
+#include "rtk/events/datasaveack.h"
+#include "rtk/events/save_to_app.h"
+#include "rtk/events/save_to_file.h"
+#include "rtk/transfer/load_lines.h"
+
+namespace rtk {
+namespace transfer {
+
+std::string& load_lines::null_sink::back()
+{
+	static std::string line;
+	return line=std::string();
+}
+
+void load_lines::null_sink::push_back(const std::string& line)
+{}
+
+void load_lines::null_sink::clear()
+{}
+
+load_lines::load_lines(size_type buffer_size):
+	load(),
+	_sink(new null_sink),
+	_buffer(new char[buffer_size]),
+	_buffer_size(buffer_size),
+	_newline(true)
+{}
+
+load_lines::~load_lines()
+{
+	delete[] _buffer;
+}
+
+void load_lines::start(size_type estsize)
+{
+	_sink->clear();
+	_newline=true;
+}
+
+void load_lines::get_block(void** data,size_type* size)
+{
+	if (data) *data=_buffer;
+	if (size) *size=_buffer_size;
+}
+
+void load_lines::put_block(size_type count)
+{
+	char* p=_buffer;
+	char* q=p;
+	for (size_type i=0;i!=count;++i)
+	{
+		if (*p++=='\n')
+		{
+			if (_newline) _sink->push_back(std::string());
+			_sink->back()+=std::string(q,p-1);
+			q=p;
+			_newline=true;
+		}
+	}
+	if (p!=q)
+	{
+		if (_newline) _sink->push_back(std::string());
+		_sink->back()+=std::string(q,p);
+		q=p;
+		_newline=false;
+	}
+}
+
+void load_lines::finish()
+{}
+
+load_lines& load_lines::clear()
+{
+	basic_sink* null=new null_sink;
+	delete _sink;
+	_sink=null;
+	return *this;
+}
+
+bool load_lines::final_newline() const
+{
+	return _final_newline;
+}
+
+load_lines& load_lines::final_newline(bool value)
+{
+	_final_newline=value;
+	return *this;
+}
+
+}; /* namespace transfer */
+}; /* namespace rtk */
---
+++ !RTK/rtk/transfer/load_lines.h
@@ -0,0 +1,166 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2004 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#ifndef _RTK_TRANSFER_LOAD_LINES
+#define _RTK_TRANSFER_LOAD_LINES
+
+#include <string>
+
+#include "rtk/transfer/load.h"
+
+namespace rtk {
+namespace transfer {
+
+/** A class for loading lines of text.
+ * Any standard container that supports push_back() and clear()
+ * may be used to hold the result.
+ */
+class load_lines:
+	public load
+{
+private:
+	/** An abstract base class to represent a line sink. */
+	class basic_sink
+	{
+	public:
+		/** Get final line.
+		 * It is an error if no lines have been appended.
+		 * @return the final line
+		 */
+		virtual std::string& back()=0;
+
+		/** Append line.
+		 * @param line the line to be appended
+		 */
+		virtual void push_back(const std::string& line)=0;
+
+		/** Erase all lines. */
+		virtual void clear()=0;
+	};
+
+	/** A class to represent a container as a line sink. */
+	template<class container>
+	class push_back_sink:
+		public basic_sink
+	{
+	private:
+		/** The container to which lines are to be appended. */
+		container& _lines;
+	public:
+		/** Construct push-back line sink.
+		 * @param lines a standard container of std::string to which
+		 *  lines are to be appended
+		 */
+		push_back_sink(container& lines);
+
+		virtual std::string& back();
+		virtual void push_back(const std::string& line);
+		virtual void clear();
+	};
+
+	/** A class to represent the null line sink. */
+	class null_sink:
+		public basic_sink
+	{
+	public:
+		virtual std::string& back();
+		virtual void push_back(const std::string& line);
+		virtual void clear();
+	};
+
+	/** A pointer to the current line sink.
+	 * This is never null.
+	 */
+	basic_sink* _sink;
+
+	/** The buffer used to receive data before it is parsed into lines. */
+	char* _buffer;
+
+	/** The size of _buffer. */
+	size_type _buffer_size;
+
+	/** The newline flag.
+	 * True if the next character parsed should begin a new line,
+	 * false if it should be appended to the previous line.
+	 */
+	bool _newline;
+
+	/** The final newline flag.
+	 * True if the final line is terminated by a newline character,
+	 * otherwise false
+	 */
+	bool _final_newline;
+public:
+	/** Construct load_lines object. */
+	load_lines(size_type buffer_size=0x400);
+
+	/** Destroy load_lines object. */
+	virtual ~load_lines();
+protected:
+	virtual void start(size_type estsize);
+	virtual void get_block(void** data,size_type* size);
+	virtual void put_block(size_type count);
+	virtual void finish();
+public:
+	/** Set line destination.
+	 * @param lines a standard container of std::string to which
+	 *  lines are to be appended
+	 */
+	template<class container>
+	load_lines& lines(container& lines);
+
+	/** Clear line destination. */
+	load_lines& clear();
+
+	/** Get final newline flag.
+	 * @return true if the final line is terminated by a newline character,
+	 *  otherwise false
+	 */
+	bool final_newline() const;
+
+	/** Set final newline flag.
+	 * @param true if the final line is terminated by a newline character,
+	 *  otherwise false
+	 */
+	load_lines& final_newline(bool value);
+};
+
+template<class container>
+load_lines::push_back_sink<container>::push_back_sink(container& lines):
+	_lines(lines)
+{}
+
+template<class container>
+std::string& load_lines::push_back_sink<container>::back()
+{
+	return _lines.back();
+}
+
+template<class container>
+void load_lines::push_back_sink<container>::push_back(const std::string& line)
+{
+	_lines.push_back(line);
+}
+
+template<class container>
+void load_lines::push_back_sink<container>::clear()
+{
+	_lines.clear();
+}
+
+template<class container>
+load_lines& load_lines::lines(container& lines)
+{
+	push_back_sink<container>* new_sink=
+		new push_back_sink<container>(lines);
+	delete _sink;
+	_sink=new_sink;
+	return *this;
+}
+
+}; /* namespace transfer */
+}; /* namespace rtk */
+
+#endif
