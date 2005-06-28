1e4b9754567a53b20b007070368d7c20956aa6e1
gdshaw <gdshaw@fa0f773a-f822-0410-a9df-e7ef4170f6f5>
2005-06-28 18:41:08 +0000

    Added class desktop::auto_filehandle.
    git-svn-id: http://source.riscpkg.org/svn/RTK/trunk@366 fa0f773a-f822-0410-a9df-e7ef4170f6f5

---
+++ !RTK/rtk/util/auto_filehandle.cc
@@ -0,0 +1,22 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2005 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#include "rtk/os/os.h"
+#include "rtk/util/auto_filehandle.h"
+
+namespace rtk {
+namespace util {
+
+auto_filehandle::auto_filehandle(int handle):
+	_handle(handle)
+{}
+
+auto_filehandle::~auto_filehandle()
+{
+	rtk::os::OS_Find0(_handle);
+}
+
+}; /* namespace util */
+}; /* namespace rtk */
---
+++ !RTK/rtk/util/auto_filehandle.h
@@ -0,0 +1,42 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2005 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#ifndef _RTK_UTIL_AUTO_FILEHANDLE
+#define _RTK_UTIL_AUTO_FILEHANDLE
+
+namespace rtk {
+namespace util {
+
+/** A class to represent a RISC OS file handle.
+ * The underlying file handle is automatically closed when
+ * the auto_filehandle is destroyed.
+ */
+class auto_filehandle
+{
+private:
+	/** The underlying RISC OS file handle. */
+	int _handle;
+public:
+	/** Create auto_filehandle.
+	 * @param handle the underlying RISC OS file handle
+	 */
+	explicit auto_filehandle(int handle);
+
+	/** Destroy auto_filehandle.
+	 * The underlying RISC OS file handle is closed.
+	 */
+	~auto_filehandle();
+
+	/** Get underlying RISC OS file handle.
+	 * @return the underlying file handle
+	 */
+	operator int() const
+		{ return _handle; }
+};
+
+}; /* namespace util */
+}; /* namespace rtk */
+
+#endif
