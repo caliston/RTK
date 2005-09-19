c7845a03fd848a461b3ce5b922942962af083196
gdshaw <gdshaw@fa0f773a-f822-0410-a9df-e7ef4170f6f5>
2005-09-19 19:20:38 +0000

    Added class rtk::desktop::wrappable_component.
    git-svn-id: http://source.riscpkg.org/svn/RTK/trunk@385 fa0f773a-f822-0410-a9df-e7ef4170f6f5

---
+++ !RTK/rtk/desktop/wrappable_component.cc
@@ -0,0 +1,50 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2005 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#include "rtk/desktop/wrappable_component.h"
+
+namespace rtk {
+namespace desktop {
+
+wrappable_component::wrappable_component(const point& origin):
+	sizeable_component(origin)
+{}
+
+wrappable_component::~wrappable_component()
+{}
+
+box wrappable_component::min_wrap_bbox(const box& wbox) const
+{
+	if (!size_valid()) resize();
+	switch (wrap_direction())
+	{
+	case wrap_horizontal:
+		if ((_min_wrap_bbox.xmin()!=wbox.xmin())||
+			(_min_wrap_bbox.xmax()!=wbox.xmax()))
+		{
+			_min_wrap_bbox=auto_wrap_bbox(wbox);
+		}
+		break;
+	case wrap_vertical:
+		if ((_min_wrap_bbox.ymin()!=wbox.ymin())||
+			(_min_wrap_bbox.ymax()!=wbox.ymax()))
+		{
+			_min_wrap_bbox=auto_wrap_bbox(wbox);
+		}
+		break;
+	case wrap_none:
+		break;
+	};
+	return _min_wrap_bbox;
+}
+
+void wrappable_component::resize() const
+{
+	inherited::resize();
+	_min_wrap_bbox=min_bbox();
+}
+
+}; /* namespace desktop */
+}; /* namespace rtk */
---
+++ !RTK/rtk/desktop/wrappable_component.h
@@ -0,0 +1,56 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2005 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#ifndef _RTK_WRAPPABLE_COMPONENT
+#define _RTK_WRAPPABLE_COMPONENT
+
+#include "rtk/desktop/sizeable_component.h"
+
+namespace rtk {
+namespace desktop {
+
+/** An abstract class from which a wrappable RTK component may be derived.
+ * An implementation of this class should provide definitions for both
+ * auto_bbox and auto_wrap_bbox.  These indicate the bounding box needed
+ * to fit the content of the component, either without or with line
+ * wrapping respectively.
+ */
+class wrappable_component:
+	public sizeable_component
+{
+private:
+	/** The class from which this one is derived. */
+	typedef sizeable_component inherited;
+private:
+	/** The line-wrapped minimum bounding box.
+	 * With line wrapping there are many possible minimum bounding boxes.
+	 * This member is used to cache one of those possibilities in order
+	 * to reduce the number of times that auto_wrap_bbox is called.
+	 */
+	mutable box _min_wrap_bbox;
+public:
+	/** Construct wrappable component.
+	 * @param origin the initial origin of the component, with respect
+	 *  to its parent
+	 */
+	wrappable_component(const point& origin=point());
+
+	/** Destroy wrappable component. */
+	virtual ~wrappable_component();
+
+	virtual box min_wrap_bbox(const box& wbox) const;
+	virtual void resize() const;
+
+	/** Get line-wrapped bounding box needed to fit content.
+	 * @return the line-wrapped bounding box needed to fit the content
+	 *  of this component
+	 */
+	virtual box auto_wrap_bbox(const box& wbox) const=0;
+};
+
+}; /* namespace desktop */
+}; /* namespace rtk */
+
+#endif
