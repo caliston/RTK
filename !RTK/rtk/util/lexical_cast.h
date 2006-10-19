62f7827feeea8f6e2324ef87a79fa3983f89d5c6
gdshaw <gdshaw@fa0f773a-f822-0410-a9df-e7ef4170f6f5>
2006-10-19 00:26:51 +0000

    Added utility function lexical_cast.
    git-svn-id: http://source.riscpkg.org/svn/RTK/trunk@390 fa0f773a-f822-0410-a9df-e7ef4170f6f5

---
+++ !RTK/rtk/util/lexical_cast.h
@@ -0,0 +1,35 @@
+// This file is part of the RISC OS Toolkit (RTK).
+// Copyright © 2006 Graham Shaw.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !RTK.Copyright.
+
+#ifndef _RTK_UTIL_LEXICAL_CAST
+#define _RTK_UTIL_LEXICAL_CAST
+
+#include <string>
+#include <sstream>
+
+namespace rtk {
+namespace util {
+
+/** Perform a lexical cast.
+ * A value of the input type is converted to the output type according to
+ * its representation as a character string.  Any characters which cannot
+ * be converted are discarded.
+ * @param value the value to be converted
+ * @return the converted value
+ */
+template<typename output_type,typename input_type>
+output_type lexical_cast(const input_type& value)
+{
+	output_type result=output_type();
+	std::stringstream buffer;
+	buffer << value;
+	buffer >> result;
+	return result;
+}
+
+}; /* namespace util */
+}; /* namespace rtk */
+
+#endif
